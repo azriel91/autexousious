9d10ffab2d403aa2ea1b73769b9a14399f88fcbd
Azriel Hoh <azriel91@gmail.com>
2018-02-08 10:30:43 +1300

    Run free_will binary headlessly in integration test
    Even though XVFB starts, creation of the window still fails with
    NoAvailablePixelFormat.
    Only some jobs will report this depending on whether stderr is captured
    before the test execution ends.
    
    Issue #1

--- app/free_will/tests/001_start_and_exit.rs
+++ app/free_will/tests/001_start_and_exit.rs
@@ -3,6 +3,13 @@ extern crate assert_cli;
 #[test]
 fn start_and_exit() {
     assert_cli::Assert::main_binary()
+        // Even though XVFB starts, creation of the window still fails with NoAvailablePixelFormat.
+        // Only some jobs will report this depending on whether stderr is captured before the
+        // test execution ends.
+        //
+        // In such a case, stdin does not get read, so we have no choice but to disable creation of
+        // the window.
+        .with_args(&["--headless"])
         .stdin("exit\n")
         .unwrap();
 }
