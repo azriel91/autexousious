220413bd5349fccec05c017d7ee64baa4c19cf5a
Azriel Hoh <mail@azriel.im>
2018-02-26 12:00:32 +1300

    Specify cargo_make specific version.

--- build/install_cargo_make.sh
+++ build/install_cargo_make.sh
@@ -7,7 +7,8 @@ is_cargo_make_installed() {
 }
 is_cargo_make_minimum() {
   # Minimum version 0.10
-  return $(cargo make --version | grep -q '\b[0-9]\+[.][0-9]\{2,\}'; echo $?)
+  # return $(cargo make --version | grep -q '\b[0-9]\+[.][0-9]\{2,\}'; echo $?)
+  return $(cargo make --version | grep -qF '0.10.3'; echo $?)
 }
 
 if ! is_cargo_make_installed; then
