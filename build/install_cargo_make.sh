4516ae57bafab19351f05d3b5d09fe6fb814c543
Azriel Hoh <mail@azriel.im>
2018-03-21 11:29:33 +1300

    Unlock `cargo-make` version to minimum 0.10
    Issue #18

--- build/install_cargo_make.sh
+++ build/install_cargo_make.sh
@@ -7,8 +7,7 @@ is_cargo_make_installed() {
 }
 is_cargo_make_minimum() {
   # Minimum version 0.10
-  # return $(cargo make --version | grep -q '\b[0-9]\+[.][0-9]\{2,\}'; echo $?)
-  return $(cargo make --version | grep -qF '0.10.5'; echo $?)
+  return $(cargo make --version | grep -q '\b[0-9]\+[.][0-9]\{2,\}'; echo $?)
 }
 
 if ! is_cargo_make_installed; then
