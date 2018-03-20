fecb27053423b83c73a61fa1c5a5762c91a87f46
Azriel Hoh <mail@azriel.im>
2018-03-21 11:54:48 +1300

    Allow `install_packages_linux` script to install packages manually.
    The manual option does not take into account concurrent processes, and
    is enabled so that non-ci users that execute it will still install the
    packages.
    
    Issue #18

--- build/install_packages_linux.sh
+++ build/install_packages_linux.sh
@@ -11,4 +11,8 @@ pkgs_desired=(
 # `apt_install` can be found at:
 #
 # <https://gitlab.com/azriel91/gitlab_runner_setup/blob/master/linux/bin/apt_install>
-apt_install "${pkgs_desired[@]}"
+if type apt_install 2>/dev/null; then
+  apt_install "${pkgs_desired[@]}"
+else
+  sudo apt update -y && sudo apt install -y "${pkgs_desired[@]}"
+fi
