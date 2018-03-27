f34802678d26d46c13b3b2fca6f113828b52fc50
Azriel Hoh <mail@azriel.im>
2018-03-27 16:47:07 +1300

    Run examples and binary under active compilation profile in tests.
    Issue #22

--- app/will/tests/001_start_and_exit.rs
+++ app/will/tests/001_start_and_exit.rs
@@ -2,5 +2,17 @@ extern crate assert_cli;
 
 #[test]
 fn start_and_exit() {
-    assert_cli::Assert::main_binary().stdin("exit\n").unwrap();
+    assert_cli::Assert::command(&main_binary_command())
+        .stdin("exit\n")
+        .unwrap();
+}
+
+// Needed while <https://github.com/assert-rs/assert_cli/issues/86> is not in `assert_cli`
+fn main_binary_command() -> Vec<&'static str> {
+    let mut command = vec!["cargo", "run"];
+    if !cfg!(debug_assertions) {
+        command.push("--release");
+    }
+    command.push("--");
+    command
 }
