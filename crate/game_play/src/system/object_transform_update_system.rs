1d94f3ddc5d776a4d432ec968669c532c16ea821
Azriel Hoh <mail@azriel.im>
2018-07-09 10:49:14 +1200

    Added tests for object kinematics and transform systems.
    Issue #41

--- crate/game_play/src/system/object_transform_update_system.rs
+++ crate/game_play/src/system/object_transform_update_system.rs
@@ -23,3 +23,60 @@ impl<'s> System<'s> for ObjectTransformUpdateSystem {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use amethyst::{
+        core::{cgmath::Vector3, transform::Transform},
+        ecs::prelude::*,
+    };
+    use amethyst_test_support::*;
+    use object_model::entity::{Kinematics, Position, Velocity};
+
+    use super::ObjectTransformUpdateSystem;
+
+    #[test]
+    fn updates_transform_with_x_and_yz() {
+        let setup = |world: &mut World| {
+            // Create entity with Kinematics
+            let position = Position::<f32>::new(-2., -2., -2.);
+            let velocity = Velocity::default();
+
+            let mut transform = Transform::default();
+            transform.translation = Vector3::new(10., 20., 0.);
+
+            let entity = world
+                .create_entity()
+                .with(Kinematics::new(position, velocity))
+                .with(transform)
+                .build();
+
+            world.add_resource(EffectReturn(entity));
+        };
+
+        let assertion = |world: &mut World| {
+            let entity = world.read_resource::<EffectReturn<Entity>>().0;
+            let store = world.read_storage::<Transform>();
+
+            let mut transform = Transform::default();
+            transform.translation = Vector3::new(-2., -4., 0.);
+
+            assert_eq!(Some(&transform), store.get(entity));
+        };
+
+        // kcov-ignore-start
+        assert!(
+            // kcov-ignore-end
+            AmethystApplication::base()
+                .with_system(
+                    ObjectTransformUpdateSystem::new(),
+                    "object_transform_update_system",
+                    &[]
+                )
+                .with_setup(setup)
+                .with_assertion(assertion)
+                .run()
+                .is_ok()
+        );
+    }
+}
