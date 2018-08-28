9803572d831987db3b3376fe3ac3db02d2402a31
Azriel Hoh <mail@azriel.im>
2018-08-29 09:13:54 +1200

    Implemented `tracker` crate to track previous component values.
    Issue #51

---
+++ crate/tracker/src/component/last.rs
@@ -0,0 +1,35 @@
+use std::ops::{Deref, DerefMut};
+
+use amethyst::ecs::prelude::*;
+use named_type::NamedType;
+
+/// Stores the last value of the component.
+#[derive(Debug, NamedType)]
+pub struct Last<T: Component + Clone + Send + Sync>(pub T);
+
+impl<T> Component for Last<T>
+where
+    T: Component + Clone + Send + Sync,
+{
+    type Storage = DenseVecStorage<Self>;
+}
+
+impl<T> Deref for Last<T>
+where
+    T: Component + Clone + Send + Sync,
+{
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for Last<T>
+where
+    T: Component + Clone + Send + Sync,
+{
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+}
