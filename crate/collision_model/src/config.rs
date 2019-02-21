10ba4c68844ffd83e834714adcaf3b54f9c207eb
Azriel Hoh <mail@azriel.im>
2019-02-22 10:58:14 +1300

    Added `Body` newtype.
    Issue #92

--- crate/collision_model/src/config.rs
+++ crate/collision_model/src/config.rs
@@ -1,9 +1,11 @@
 //! Types representing collision configuration.
 
 pub use self::{
-    body_frame::BodyFrame, interaction::Interaction, interaction_frame::InteractionFrame,
+    body::Body, body_frame::BodyFrame, interaction::Interaction,
+    interaction_frame::InteractionFrame,
 };
 
+mod body;
 mod body_frame;
 mod interaction;
 mod interaction_frame;
---
+++ crate/collision_model/src/config/body.rs
@@ -0,0 +1,126 @@
+use amethyst::{
+    assets::{Asset, Handle, ProcessingState},
+    ecs::VecStorage,
+    Error,
+};
+use derive_deref::{Deref, DerefMut};
+use derive_new::new;
+use serde::{Deserialize, Serialize};
+use shape_model::Volume;
+
+/// Hittable volumes of an interactable object.
+#[derive(Clone, Debug, Default, Deref, DerefMut, Deserialize, PartialEq, Eq, Serialize, new)]
+pub struct Body(
+    /// Backing vector of `Volume`s.
+    #[serde(default)]
+    pub Vec<Volume>,
+);
+
+impl Asset for Body {
+    const NAME: &'static str = concat!(module_path!(), "::", stringify!(Body));
+    type Data = Self;
+    type HandleStorage = VecStorage<Handle<Self>>;
+}
+
+impl From<Body> for Result<ProcessingState<Body>, Error> {
+    fn from(body: Body) -> Result<ProcessingState<Body>, Error> {
+        Ok(ProcessingState::Loaded(body))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde::Deserialize;
+    use shape_model::{Axis, Volume};
+    use toml;
+
+    use super::Body;
+
+    const BODY_ALL_SPECIFIED: &str = r#"
+        body = [
+          { box = { x = -1, y = -2, z = -3, w = 11, h = 12, d = 13 } },
+          { cylinder = { axis = "x", center = -4, r = 14, l = 24 } },
+          { cylinder = { axis = "y", center = -5, r = 15, l = 25 } },
+          { cylinder = { axis = "z", center = -6, r = 16, l = 26 } },
+          { sphere = { x = -7, y = -8, z = -9, r = 17 } },
+        ]
+    "#;
+    const BODY_MINIMUM_SPECIFIED: &str = r#"
+        body = [
+          { box = { x = -1, y = -2, w = 11, h = 12 } },
+          { sphere = { x = -7, y = -8, r = 17 } },
+        ]
+    "#;
+
+    #[test]
+    fn body_specify_all_fields() {
+        let frame =
+            toml::from_str::<BodyFrame>(BODY_ALL_SPECIFIED).expect("Failed to deserialize frame.");
+
+        let body_volumes = vec![
+            Volume::Box {
+                x: -1,
+                y: -2,
+                z: -3,
+                w: 11,
+                h: 12,
+                d: 13,
+            },
+            Volume::Cylinder {
+                axis: Axis::X,
+                center: -4,
+                r: 14,
+                l: 24,
+            },
+            Volume::Cylinder {
+                axis: Axis::Y,
+                center: -5,
+                r: 15,
+                l: 25,
+            },
+            Volume::Cylinder {
+                axis: Axis::Z,
+                center: -6,
+                r: 16,
+                l: 26,
+            },
+            Volume::Sphere {
+                x: -7,
+                y: -8,
+                z: -9,
+                r: 17,
+            },
+        ];
+        assert_eq!(Body::new(body_volumes), frame.body);
+    }
+
+    #[test]
+    fn body_specify_minimum_fields() {
+        let frame = toml::from_str::<BodyFrame>(BODY_MINIMUM_SPECIFIED)
+            .expect("Failed to deserialize frame.");
+
+        let body_volumes = vec![
+            Volume::Box {
+                x: -1,
+                y: -2,
+                z: 0,
+                w: 11,
+                h: 12,
+                d: 26,
+            },
+            Volume::Sphere {
+                x: -7,
+                y: -8,
+                z: 0,
+                r: 17,
+            },
+        ];
+        assert_eq!(Body::new(body_volumes), frame.body);
+    }
+
+    /// Needed because the TOML deserializer does not support deserializing values directly.
+    #[derive(Debug, Deserialize)]
+    struct BodyFrame {
+        body: Body,
+    }
+}
