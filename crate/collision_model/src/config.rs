057d82001e8ab30530183516dfa4032b0a62676b
Azriel Hoh <mail@azriel.im>
2019-02-22 11:04:55 +1300

    Added `Interactions` newtype.
    Issue #92

--- crate/collision_model/src/config.rs
+++ crate/collision_model/src/config.rs
@@ -2,10 +2,11 @@
 
 pub use self::{
     body::Body, body_frame::BodyFrame, interaction::Interaction,
-    interaction_frame::InteractionFrame,
+    interaction_frame::InteractionFrame, interactions::Interactions,
 };
 
 mod body;
 mod body_frame;
 mod interaction;
 mod interaction_frame;
+mod interactions;
