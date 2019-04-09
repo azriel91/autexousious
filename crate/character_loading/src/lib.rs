cdc3a9ef874a0f3be4e708471b782c83af611e33
Azriel Hoh <mail@azriel.im>
2019-04-09 15:32:25 +1200

    Removed unused `CharacterLoader` code.
    Issue #96

--- crate/character_loading/src/lib.rs
+++ crate/character_loading/src/lib.rs
@@ -4,13 +4,11 @@
 //! Processes character configuration into the loaded character model.
 
 pub use crate::{
-    character_loader::CharacterLoader,
     character_loading_bundle::{CharacterLoadingBundle, CHARACTER_PREFAB_LOADER_SYSTEM},
     prefab::{CharacterEntityAugmenter, CharacterPrefab, CharacterPrefabHandle},
     system_data::CharacterComponentStorages,
 };
 
-mod character_loader;
 mod character_loading_bundle;
 mod prefab;
 mod system_data;
