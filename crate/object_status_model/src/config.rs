3d6adeb6f577284dd030500e9e52fefcccde8123
Azriel Hoh <mail@azriel.im>
2019-05-21 08:49:15 +1200

    Added `StunPoints` in `object_status_model` crate.
    Issue #118

---
+++ crate/object_status_model/src/config.rs
@@ -0,0 +1,5 @@
+//! Contains the types that represent the configuration on disk.
+
+pub use self::stun_points::{StunPoints, STUN_POINTS_DEFAULT};
+
+mod stun_points;
---
+++ crate/object_status_model/src/config/stun_points.rs
@@ -0,0 +1,16 @@
+use amethyst::ecs::{storage::VecStorage, Component};
+use derivative::Derivative;
+use derive_more::{Add, AddAssign, Display, From, Sub, SubAssign};
+use numeric_newtype_derive::numeric_newtype;
+use serde::{Deserialize, Serialize};
+use specs_derive::Component;
+
+/// Default stun to inflict on hit.
+pub const STUN_POINTS_DEFAULT: u32 = 10;
+
+/// Stun points of an object.
+#[numeric_newtype]
+#[derive(Component, Debug, Derivative, Deserialize, Hash, Serialize)]
+#[derivative(Default)]
+#[storage(VecStorage)]
+pub struct StunPoints(#[derivative(Default(value = "STUN_POINTS_DEFAULT"))] pub u32);
