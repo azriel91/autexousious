189bc77366c0273091eca61d4d6701b237ecf422
Azriel Hoh <mail@azriel.im>
2019-06-07 11:45:49 +1200

    Updated `character_selection_stdio` for event model changes.
    Issue #135

--- crate/character_selection_stdio/src/character_selection_event_args.rs
+++ crate/character_selection_stdio/src/character_selection_event_args.rs
@@ -5,13 +5,46 @@ use structopt_derive::StructOpt;
 ///
 /// # Examples
 ///
-/// * `character_selection select -c 1 -s default/heat`
-/// * `character_selection deselect -c 1`
+/// * `character_selection return`
+/// * `character_selection join -c 0`
+/// * `character_selection leave -c 0`
+/// * `character_selection switch -c 0 -s default/heat`
+/// * `character_selection select -c 0 -s default/heat`
+/// * `character_selection deselect -c 0`
 /// * `character_selection confirm`
 #[derive(Clone, Debug, PartialEq, StructOpt)]
 #[structopt(rename_all = "snake_case")]
 pub enum CharacterSelectionEventArgs {
-    /// Select event.
+    /// Signal to return from `CharacterSelectionState`.
+    Return,
+    /// Player has joined / become active.
+    Join {
+        /// Controller ID.
+        ///
+        /// 0 for the first player, 1 for the second player, etcetera.
+        #[structopt(short, long)]
+        controller_id: ControllerId,
+    },
+    /// Player has left / become inactive.
+    Leave {
+        /// Controller ID.
+        ///
+        /// 0 for the first player, 1 for the second player, etcetera.
+        #[structopt(short, long)]
+        controller_id: ControllerId,
+    },
+    /// Character has been selected.
+    Switch {
+        /// Controller ID.
+        ///
+        /// 0 for the first player, 1 for the second player, etcetera.
+        #[structopt(short, long)]
+        controller_id: ControllerId,
+        /// Slug of the character or random, e.g. "default/heat", "random".
+        #[structopt(short, long)]
+        selection: String,
+    },
+    /// Character has been selected.
     Select {
         /// Controller ID.
         ///
@@ -22,7 +55,7 @@ pub enum CharacterSelectionEventArgs {
         #[structopt(short, long)]
         selection: String,
     },
-    /// Deselect event.
+    /// Character has been deselected.
     Deselect {
         /// Controller ID.
         ///
@@ -30,6 +63,6 @@ pub enum CharacterSelectionEventArgs {
         #[structopt(short, long)]
         controller_id: ControllerId,
     },
-    /// Confirm event.
+    /// Character selections have been confirmed.
     Confirm,
 }
--- crate/character_selection_stdio/src/character_selection_event_stdin_mapper.rs
+++ crate/character_selection_stdio/src/character_selection_event_stdin_mapper.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use amethyst::{ecs::Read, Error};
-use asset_model::{config::AssetSlug, loaded::SlugAndHandle};
+use asset_model::config::AssetSlug;
 use character_selection_model::{CharacterSelection, CharacterSelectionEvent};
 use game_input_model::ControllerId;
 use game_model::loaded::CharacterAssets;
@@ -9,6 +9,9 @@ use stdio_spi::{MapperSystemData, StdinMapper, StdioError};
 use typename_derive::TypeName;
 
 use crate::CharacterSelectionEventArgs;
+
+/// Magic string to indicate `random` selection.
+const RANDOM_SELECTION: &str = "random";
 
 #[derive(Debug)]
 pub struct CharacterSelectionEventStdinMapperData;
@@ -22,33 +25,27 @@ impl<'s> MapperSystemData<'s> for CharacterSelectionEventStdinMapperData {
 pub struct CharacterSelectionEventStdinMapper;
 
 impl CharacterSelectionEventStdinMapper {
+    fn map_switch_event(
+        character_assets: &CharacterAssets,
+        controller_id: ControllerId,
+        selection: &str,
+    ) -> Result<CharacterSelectionEvent, Error> {
+        let character_selection = Self::find_character(character_assets, selection)?;
+
+        let character_selection_event = CharacterSelectionEvent::Switch {
+            controller_id,
+            character_selection,
+        };
+
+        Ok(character_selection_event)
+    }
+
     fn map_select_event(
         character_assets: &CharacterAssets,
         controller_id: ControllerId,
         selection: &str,
     ) -> Result<CharacterSelectionEvent, Error> {
-        let character_selection = match selection {
-            "random" => {
-                let snh = SlugAndHandle::from(
-                    character_assets
-                        .iter()
-                        .next()
-                        .expect("Expected at least one character to be loaded."),
-                );
-                CharacterSelection::Random(snh)
-            }
-            slug_str => {
-                let slug = AssetSlug::from_str(slug_str).map_err(StdioError::Msg)?;
-                let handle = character_assets
-                    .get(&slug)
-                    .ok_or_else(|| format!("No character found with asset slug `{}`.", slug))
-                    .map_err(StdioError::Msg)?
-                    .clone();
-
-                let snh = SlugAndHandle { slug, handle };
-                CharacterSelection::Id(snh)
-            }
-        };
+        let character_selection = Self::find_character(character_assets, selection)?;
 
         let character_selection_event = CharacterSelectionEvent::Select {
             controller_id,
@@ -56,6 +53,26 @@ impl CharacterSelectionEventStdinMapper {
         };
 
         Ok(character_selection_event)
+    }
+
+    fn find_character(
+        character_assets: &CharacterAssets,
+        selection: &str,
+    ) -> Result<CharacterSelection, Error> {
+        match selection {
+            RANDOM_SELECTION => Ok(CharacterSelection::Random),
+            slug_str => {
+                let slug = AssetSlug::from_str(slug_str).map_err(StdioError::Msg)?;
+
+                // TODO: Should we validate here, or in `CharacterSelectionSpawningSystem`?
+                let _ = character_assets
+                    .get(&slug)
+                    .ok_or_else(|| format!("No character found with asset slug `{}`.", slug))
+                    .map_err(StdioError::Msg)?;
+
+                Ok(CharacterSelection::Id(slug))
+            }
+        }
     }
 }
 
@@ -69,12 +86,23 @@ impl StdinMapper for CharacterSelectionEventStdinMapper {
         args: Self::Args,
     ) -> Result<Self::Event, Error> {
         match args {
+            CharacterSelectionEventArgs::Return => Ok(CharacterSelectionEvent::Return),
+            CharacterSelectionEventArgs::Switch {
+                controller_id,
+                selection,
+            } => Self::map_switch_event(character_assets, controller_id, &selection),
             CharacterSelectionEventArgs::Select {
                 controller_id,
                 selection,
             } => Self::map_select_event(character_assets, controller_id, &selection),
             CharacterSelectionEventArgs::Deselect { controller_id } => {
                 Ok(CharacterSelectionEvent::Deselect { controller_id })
+            }
+            CharacterSelectionEventArgs::Join { controller_id } => {
+                Ok(CharacterSelectionEvent::Join { controller_id })
+            }
+            CharacterSelectionEventArgs::Leave { controller_id } => {
+                Ok(CharacterSelectionEvent::Leave { controller_id })
             }
             CharacterSelectionEventArgs::Confirm => Ok(CharacterSelectionEvent::Confirm),
         }
@@ -88,7 +116,6 @@ mod tests {
         Error,
     };
     use application_test_support::AutexousiousApplication;
-    use asset_model::loaded::SlugAndHandle;
     use assets_test::ASSETS_CHAR_BAT_SLUG;
     use character_selection_model::{CharacterSelection, CharacterSelectionEvent};
     use game_model::loaded::CharacterAssets;
@@ -97,159 +124,174 @@ mod tests {
     use super::CharacterSelectionEventStdinMapper;
     use crate::CharacterSelectionEventArgs;
 
-    #[test]
-    fn returns_err_when_asset_slug_invalid() {
-        let controller_id = 0;
-        let selection = "invalid".to_string();
-        let args = CharacterSelectionEventArgs::Select {
-            controller_id,
-            selection,
+    macro_rules! test_map_direct {
+        ($test_name:ident, $variant:ident) => {
+            #[test]
+            fn $test_name() {
+                let args = CharacterSelectionEventArgs::$variant;
+                let mut resources = Resources::new();
+                resources.insert(CharacterAssets::new());
+
+                let result = CharacterSelectionEventStdinMapper::map(
+                    &Read::from(resources.fetch::<CharacterAssets>()),
+                    args,
+                );
+
+                assert!(result.is_ok());
+                assert_eq!(CharacterSelectionEvent::$variant, result.unwrap())
+            }
         };
-        let mut resources = Resources::new();
-        resources.insert(CharacterAssets::new());
-
-        let result = CharacterSelectionEventStdinMapper::map(
-            &Read::from(resources.fetch::<CharacterAssets>()),
-            args,
-        );
-
-        expect_err_msg(
-            result,
-            "Expected exactly one `/` in slug string: \"invalid\".",
-        );
     }
 
-    #[test]
-    fn returns_err_when_character_does_not_exist_for_slug() {
-        let controller_id = 0;
-        let selection = "test/non_existent".to_string();
-        let args = CharacterSelectionEventArgs::Select {
-            controller_id,
-            selection,
+    macro_rules! test_map_with_controller_id {
+        ($test_name:ident, $variant:ident) => {
+            #[test]
+            fn $test_name() {
+                let controller_id = 0;
+                let args = CharacterSelectionEventArgs::$variant { controller_id };
+                let mut resources = Resources::new();
+                resources.insert(CharacterAssets::new());
+
+                let result = CharacterSelectionEventStdinMapper::map(
+                    &Read::from(resources.fetch::<CharacterAssets>()),
+                    args,
+                );
+
+                assert!(result.is_ok());
+                assert_eq!(
+                    CharacterSelectionEvent::$variant { controller_id },
+                    result.unwrap()
+                )
+            }
         };
-        let mut resources = Resources::new();
-        resources.insert(CharacterAssets::new());
-
-        let result = CharacterSelectionEventStdinMapper::map(
-            &Read::from(resources.fetch::<CharacterAssets>()),
-            args,
-        );
-
-        expect_err_msg(
-            result,
-            "No character found with asset slug `test/non_existent`.",
-        );
     }
 
-    #[test]
-    fn maps_select_id_event() {
-        // kcov-ignore-start
-        assert!(
-            // kcov-ignore-end
-            AutexousiousApplication::config_base()
-                .with_assertion(|world| {
-                    let controller_id = 1;
-                    let args = CharacterSelectionEventArgs::Select {
-                        controller_id,
-                        selection: ASSETS_CHAR_BAT_SLUG.to_string(),
-                    };
-                    let character_assets = world.read_resource::<CharacterAssets>();
-                    let snh =
-                        SlugAndHandle::from((&*character_assets, ASSETS_CHAR_BAT_SLUG.clone()));
-
-                    let result = CharacterSelectionEventStdinMapper::map(
-                        &Read::from(character_assets),
-                        args,
-                    );
-
-                    assert!(result.is_ok());
-                    let character_selection = CharacterSelection::Id(snh);
-                    assert_eq!(
-                        CharacterSelectionEvent::Select {
+    macro_rules! test_map_with_slug {
+        ($test_name:ident, $variant:ident, $slug:expr, $selection:expr) => {
+            #[test]
+            fn $test_name() -> Result<(), Error> {
+                AutexousiousApplication::config_base()
+                    .with_assertion(|world| {
+                        let controller_id = 1;
+                        let args = CharacterSelectionEventArgs::$variant {
                             controller_id,
-                            character_selection
-                        },
-                        result.unwrap()
-                    )
-                })
-                .run_isolated()
-                .is_ok()
-        );
+                            selection: $slug,
+                        };
+                        let character_assets = world.read_resource::<CharacterAssets>();
+
+                        let result = CharacterSelectionEventStdinMapper::map(
+                            &Read::from(character_assets),
+                            args,
+                        );
+
+                        assert!(result.is_ok());
+                        let character_selection = $selection;
+                        assert_eq!(
+                            CharacterSelectionEvent::$variant {
+                                controller_id,
+                                character_selection
+                            },
+                            result.unwrap()
+                        )
+                    })
+                    .run_isolated()
+            }
+        };
     }
 
-    #[test]
-    fn maps_select_random_event() {
-        // kcov-ignore-start
-        assert!(
-            // kcov-ignore-end
-            AutexousiousApplication::config_base()
-                .with_assertion(|world| {
-                    let controller_id = 1;
-                    let args = CharacterSelectionEventArgs::Select {
-                        controller_id,
-                        selection: "random".to_string(),
-                    };
-                    let character_assets = world.read_resource::<CharacterAssets>();
-                    let snh = SlugAndHandle::from(
-                        character_assets
-                            .iter()
-                            .next()
-                            .expect("Expected at least one character to be loaded."),
-                    );
+    macro_rules! test_slug_invalid {
+        ($test_name:ident, $variant:ident) => {
+            #[test]
+            fn $test_name() {
+                let controller_id = 0;
+                let selection = "invalid".to_string();
+                let args = CharacterSelectionEventArgs::$variant {
+                    controller_id,
+                    selection,
+                };
+                let mut resources = Resources::new();
+                resources.insert(CharacterAssets::new());
 
-                    let result = CharacterSelectionEventStdinMapper::map(
-                        &Read::from(character_assets),
-                        args,
-                    );
+                let result = CharacterSelectionEventStdinMapper::map(
+                    &Read::from(resources.fetch::<CharacterAssets>()),
+                    args,
+                );
 
-                    assert!(result.is_ok());
-                    let character_selection = CharacterSelection::Random(snh);
-                    assert_eq!(
-                        CharacterSelectionEvent::Select {
-                            controller_id,
-                            character_selection
-                        },
-                        result.unwrap()
-                    )
-                })
-                .run_isolated()
-                .is_ok()
-        );
+                expect_err_msg(
+                    result,
+                    "Expected exactly one `/` in slug string: \"invalid\".",
+                );
+            }
+        };
     }
 
-    #[test]
-    fn maps_deselect_event() {
-        let controller_id = 0;
-        let args = CharacterSelectionEventArgs::Deselect { controller_id };
-        let mut resources = Resources::new();
-        resources.insert(CharacterAssets::new());
+    macro_rules! test_err_when_char_not_exist {
+        ($test_name:ident, $variant:ident) => {
+            #[test]
+            fn $test_name() {
+                let controller_id = 0;
+                let selection = "test/non_existent".to_string();
+                let args = CharacterSelectionEventArgs::$variant {
+                    controller_id,
+                    selection,
+                };
+                let mut resources = Resources::new();
+                resources.insert(CharacterAssets::new());
 
-        let result = CharacterSelectionEventStdinMapper::map(
-            &Read::from(resources.fetch::<CharacterAssets>()),
-            args,
-        );
+                let result = CharacterSelectionEventStdinMapper::map(
+                    &Read::from(resources.fetch::<CharacterAssets>()),
+                    args,
+                );
 
-        assert!(result.is_ok());
-        assert_eq!(
-            CharacterSelectionEvent::Deselect { controller_id },
-            result.unwrap()
-        )
+                expect_err_msg(
+                    result,
+                    "No character found with asset slug `test/non_existent`.",
+                );
+            }
+        };
     }
 
-    #[test]
-    fn maps_confirm_event() {
-        let args = CharacterSelectionEventArgs::Confirm;
-        let mut resources = Resources::new();
-        resources.insert(CharacterAssets::new());
+    test_slug_invalid!(returns_err_when_asset_slug_invalid_switch, Switch);
+    test_slug_invalid!(returns_err_when_asset_slug_invalid_select, Select);
+    test_err_when_char_not_exist!(
+        returns_err_when_character_does_not_exist_for_slug_switch,
+        Switch
+    );
+    test_err_when_char_not_exist!(
+        returns_err_when_character_does_not_exist_for_slug_select,
+        Select
+    );
 
-        let result = CharacterSelectionEventStdinMapper::map(
-            &Read::from(resources.fetch::<CharacterAssets>()),
-            args,
-        );
+    test_map_with_slug!(
+        maps_select_id_event,
+        Select,
+        ASSETS_CHAR_BAT_SLUG.to_string(),
+        CharacterSelection::Id(ASSETS_CHAR_BAT_SLUG.clone())
+    );
+    test_map_with_slug!(
+        maps_select_random_event,
+        Select,
+        String::from("random"),
+        CharacterSelection::Random
+    );
+    test_map_with_slug!(
+        maps_switch_id_event,
+        Switch,
+        ASSETS_CHAR_BAT_SLUG.to_string(),
+        CharacterSelection::Id(ASSETS_CHAR_BAT_SLUG.clone())
+    );
+    test_map_with_slug!(
+        maps_switch_random_event,
+        Switch,
+        String::from("random"),
+        CharacterSelection::Random
+    );
 
-        assert!(result.is_ok());
-        assert_eq!(CharacterSelectionEvent::Confirm, result.unwrap())
-    }
+    test_map_with_controller_id!(maps_join_event, Join);
+    test_map_with_controller_id!(maps_leave_event, Leave);
+    test_map_with_controller_id!(maps_deselect_event, Deselect);
+    test_map_direct!(maps_return_event, Return);
+    test_map_direct!(maps_confirm_event, Confirm);
 
     fn expect_err_msg(result: Result<CharacterSelectionEvent, Error>, expected: &str) {
         assert!(result.is_err());
