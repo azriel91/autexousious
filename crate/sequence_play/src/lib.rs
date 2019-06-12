adb046974dd77ecbaad4a0eda78d138ac0c2a850
Azriel Hoh <mail@azriel.im>
2019-06-12 15:04:30 +1200

    Updated `SequenceUpdateSystem` to use `WaitSequenceHandle`s.
    Issue #111

---
+++ crate/sequence_play/src/lib.rs
@@ -0,0 +1,7 @@
+#![deny(missing_debug_implementations, missing_docs)] // kcov-ignore
+
+//! Provides logic to update sequences.
+
+pub use crate::system::SequenceUpdateSystem;
+
+mod system;
---
+++ crate/sequence_play/src/system.rs
@@ -0,0 +1,3 @@
+pub use self::sequence_update_system::SequenceUpdateSystem;
+
+mod sequence_update_system;
