e695bffe0ceb18c2b74e0bc3298e6993ea3779cc
Azriel Hoh <mail@azriel.im>
2019-06-27 11:40:12 +1200

    Added `Team` component to spawned characters.
    Issue #140

--- crate/game_loading/src/system/character_selection_spawning_system.rs
+++ crate/game_loading/src/system/character_selection_spawning_system.rs
@@ -1,10 +1,13 @@
 use amethyst::ecs::{Entities, Entity, Read, System, Write, WriteStorage};
 use character_prefab::CharacterPrefabHandle;
 use character_selection_model::CharacterSelections;
+use derivative::Derivative;
 use derive_new::new;
 use game_input::InputControlled;
 use game_model::{loaded::CharacterPrefabs, play::GameEntities};
 use object_type::ObjectType;
+use shred_derive::SystemData;
+use team_model::play::{IndependentCounter, Team};
 use typename_derive::TypeName;
 
 use crate::{CharacterAugmentStatus, GameLoadingStatus};
@@ -13,30 +16,54 @@ use crate::{CharacterAugmentStatus, GameLoadingStatus};
 #[derive(Debug, Default, TypeName, new)]
 pub(crate) struct CharacterSelectionSpawningSystem;
 
-type CharacterSelectionSpawningSystemData<'s> = (
-    Entities<'s>,
-    Read<'s, CharacterSelections>,
-    Read<'s, CharacterPrefabs>,
-    Write<'s, GameLoadingStatus>,
-    WriteStorage<'s, CharacterPrefabHandle>,
-    WriteStorage<'s, InputControlled>,
-    Write<'s, GameEntities>,
-);
+#[derive(Derivative, SystemData)]
+#[derivative(Debug)]
+pub struct CharacterSelectionSpawningSystemData<'s> {
+    /// `Entities` resource.
+    #[derivative(Debug = "ignore")]
+    pub entities: Entities<'s>,
+    /// `CharacterSelections` resource.
+    #[derivative(Debug = "ignore")]
+    pub character_selections: Read<'s, CharacterSelections>,
+    /// `CharacterPrefabs` resource.
+    #[derivative(Debug = "ignore")]
+    pub character_prefabs: Read<'s, CharacterPrefabs>,
+    /// `GameLoadingStatus` resource.
+    #[derivative(Debug = "ignore")]
+    pub game_loading_status: Write<'s, GameLoadingStatus>,
+    /// `IndependentCounter` resource.
+    #[derivative(Debug = "ignore")]
+    pub independent_counter: Write<'s, IndependentCounter>,
+    /// `CharacterPrefabHandle` components.
+    #[derivative(Debug = "ignore")]
+    pub character_prefab_handles: WriteStorage<'s, CharacterPrefabHandle>,
+    /// `InputControlled` components.
+    #[derivative(Debug = "ignore")]
+    pub input_controlleds: WriteStorage<'s, InputControlled>,
+    /// `Team` components.
+    #[derivative(Debug = "ignore")]
+    pub teams: WriteStorage<'s, Team>,
+    /// `GameEntities` resource.
+    #[derivative(Debug = "ignore")]
+    pub game_entities: Write<'s, GameEntities>,
+}
 
 impl<'s> System<'s> for CharacterSelectionSpawningSystem {
     type SystemData = CharacterSelectionSpawningSystemData<'s>;
 
     fn run(
         &mut self,
-        (
+        CharacterSelectionSpawningSystemData {
             entities,
             character_selections,
             character_prefabs,
             mut game_loading_status,
+            mut independent_counter,
             mut character_prefab_handles,
             mut input_controlleds,
+            mut teams,
             mut game_entities,
-        ): Self::SystemData,
+        }: Self::SystemData,
     ) {
         if game_loading_status.character_augment_status != CharacterAugmentStatus::Prefab {
             return;
@@ -60,11 +87,16 @@ impl<'s> System<'s> for CharacterSelectionSpawningSystem {
 
                 input_controlleds
                     .insert(entity, InputControlled::new(*controller_id))
-                    .expect("Failed to insert input_controlled for character.");
-
+                    .expect("Failed to insert `InputControlled` for character.");
                 character_prefab_handles
                     .insert(entity, handle)
-                    .expect("Failed to insert character_prefab_handle for character.");
+                    .expect("Failed to insert `CharacterPrefabHandle` for character.");
+                teams
+                    .insert(
+                        entity,
+                        Team::Independent(independent_counter.get_and_increment()),
+                    )
+                    .expect("Failed to insert `Team` for character.");
 
                 entity
             })
@@ -80,27 +112,26 @@ impl<'s> System<'s> for CharacterSelectionSpawningSystem {
 
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashMap, env};
+    use std::env;
 
     use amethyst::{
         assets::Processor,
         audio::Source,
         core::TransformBundle,
-        ecs::{Builder, Entity},
+        ecs::{Join, ReadStorage, System, SystemData, World},
         renderer::{types::DefaultBackend, RenderEmptyBundle},
         window::ScreenDimensions,
         Error,
     };
-    use amethyst_test::{
-        AmethystApplication, EffectReturn, PopState, HIDPI, SCREEN_HEIGHT, SCREEN_WIDTH,
-    };
+    use amethyst_test::{AmethystApplication, PopState, HIDPI, SCREEN_HEIGHT, SCREEN_WIDTH};
     use application_event::{AppEvent, AppEventReader};
     use assets_test::{ASSETS_PATH, CHAR_BAT_SLUG};
     use character_loading::{CharacterLoadingBundle, CHARACTER_PROCESSOR};
-    use character_prefab::CharacterPrefabBundle;
+    use character_prefab::{CharacterPrefabBundle, CharacterPrefabHandle};
     use character_selection_model::CharacterSelections;
     use collision_audio_loading::CollisionAudioLoadingBundle;
     use collision_loading::CollisionLoadingBundle;
+    use game_input::InputControlled;
     use game_input_model::ControlBindings;
     use game_model::play::GameEntities;
     use loading::{LoadingBundle, LoadingState};
@@ -109,6 +140,7 @@ mod tests {
     use sequence_loading::SequenceLoadingBundle;
     use spawn_loading::SpawnLoadingBundle;
     use sprite_loading::SpriteLoadingBundle;
+    use team_model::play::{IndependentCounter, Team};
     use typename::TypeName;
     use ui_audio_loading::UiAudioLoadingBundle;
 
@@ -117,113 +149,132 @@ mod tests {
 
     #[test]
     fn returns_if_augment_status_is_not_prefab() -> Result<(), Error> {
-        AmethystApplication::blank()
-            .with_bundle(TransformBundle::new())
-            .with_bundle(RenderEmptyBundle::<DefaultBackend>::new())
-            .with_custom_event_type::<AppEvent, AppEventReader>()
-            .with_resource(ScreenDimensions::new(SCREEN_WIDTH, SCREEN_HEIGHT, HIDPI))
-            .with_ui_bundles::<ControlBindings>()
-            .with_system(Processor::<Source>::new(), "source_processor", &[])
-            .with_bundle(SpriteLoadingBundle::new())
-            .with_bundle(SequenceLoadingBundle::new())
-            .with_bundle(LoadingBundle::new(ASSETS_PATH.clone()))
-            .with_bundle(CollisionLoadingBundle::new())
-            .with_bundle(SpawnLoadingBundle::new())
-            .with_bundle(MapLoadingBundle::new())
-            .with_bundle(CharacterLoadingBundle::new())
-            .with_bundle(
-                CharacterPrefabBundle::new()
-                    .with_system_dependencies(&[String::from(CHARACTER_PROCESSOR)]),
-            )
-            .with_bundle(CollisionAudioLoadingBundle::new(ASSETS_PATH.clone()))
-            .with_bundle(UiAudioLoadingBundle::new(ASSETS_PATH.clone()))
-            .with_state(|| LoadingState::new(PopState))
-            .with_setup(|world| {
+        run_test(
+            |world| {
                 let mut game_loading_status = GameLoadingStatus::new();
                 game_loading_status.character_augment_status = CharacterAugmentStatus::Rectify;
                 world.add_resource(game_loading_status);
 
-                let char_entity = world.create_entity().build();
-                let mut objects = HashMap::new();
-                objects.insert(ObjectType::Character, vec![char_entity.clone()]);
-
-                world.add_resource(GameEntities::new(objects, Vec::new()));
-                world.add_resource(EffectReturn(char_entity));
-            })
-            .with_system_single(
-                CharacterSelectionSpawningSystem,
-                CharacterSelectionSpawningSystem::type_name(),
-                &[],
-            ) // kcov-ignore
-            .with_assertion(|world| {
-                let char_entity = &world.read_resource::<EffectReturn<Entity>>().0;
-                assert_eq!(
-                    char_entity,
-                    world
-                        .read_resource::<GameEntities>()
-                        .objects
-                        .get(&ObjectType::Character)
-                        .expect("Expected `ObjectType::Character` key in `GameEntities`.")
-                        .iter()
-                        .next()
-                        .expect("Expected characters to have an entity.")
-                );
-            })
-            .run_isolated()
-    }
-
-    #[test]
-    fn spawns_characters_when_they_havent_been_spawned() -> Result<(), Error> {
-        env::set_var("APP_DIR", env!("CARGO_MANIFEST_DIR"));
-
-        AmethystApplication::blank()
-            .with_bundle(TransformBundle::new())
-            .with_resource(ScreenDimensions::new(SCREEN_WIDTH, SCREEN_HEIGHT, HIDPI))
-            .with_bundle(RenderEmptyBundle::<DefaultBackend>::new())
-            .with_custom_event_type::<AppEvent, AppEventReader>()
-            .with_resource(ScreenDimensions::new(SCREEN_WIDTH, SCREEN_HEIGHT, HIDPI))
-            .with_ui_bundles::<ControlBindings>()
-            .with_system(Processor::<Source>::new(), "source_processor", &[])
-            .with_bundle(SpriteLoadingBundle::new())
-            .with_bundle(SequenceLoadingBundle::new())
-            .with_bundle(LoadingBundle::new(ASSETS_PATH.clone()))
-            .with_bundle(CollisionLoadingBundle::new())
-            .with_bundle(SpawnLoadingBundle::new())
-            .with_bundle(MapLoadingBundle::new())
-            .with_bundle(CharacterLoadingBundle::new())
-            .with_bundle(
-                CharacterPrefabBundle::new()
-                    .with_system_dependencies(&[String::from(CHARACTER_PROCESSOR)]),
-            )
-            .with_bundle(CollisionAudioLoadingBundle::new(ASSETS_PATH.clone()))
-            .with_bundle(UiAudioLoadingBundle::new(ASSETS_PATH.clone()))
-            .with_state(|| LoadingState::new(PopState))
-            .with_setup(|world| {
                 let mut character_selections = CharacterSelections::default();
                 character_selections
                     .selections
                     .insert(0, CHAR_BAT_SLUG.clone());
                 world.add_resource(character_selections);
-            })
-            .with_system_single(
-                CharacterSelectionSpawningSystem,
-                CharacterSelectionSpawningSystem::type_name(),
-                &[],
-            ) // kcov-ignore
-            .with_assertion(|world| {
-                assert!(!world
-                    .read_resource::<GameEntities>()
-                    .objects
-                    .get(&ObjectType::Character)
-                    .expect("Expected `ObjectType::Character` key in `GameEntities`.")
-                    .is_empty());
+            },
+            |world| {
+                let (input_controlleds, character_prefab_handles, teams) =
+                    world.system_data::<TestSystemData<'_>>();
+                assert_eq!(0, input_controlleds.count());
+                assert_eq!(0, character_prefab_handles.count());
+                assert_eq!(0, teams.count());
+            },
+        )
+    }
+
+    #[test]
+    fn spawns_characters_when_they_havent_been_spawned() -> Result<(), Error> {
+        run_test(
+            |world| {
+                let mut character_selections = CharacterSelections::default();
+                character_selections
+                    .selections
+                    .insert(0, CHAR_BAT_SLUG.clone());
+                character_selections
+                    .selections
+                    .insert(123, CHAR_BAT_SLUG.clone());
+                world.add_resource(character_selections);
+            },
+            |world| {
+                let (input_controlleds, character_prefab_handles, teams) =
+                    world.system_data::<TestSystemData<'_>>();
+                let components = (&input_controlleds, &character_prefab_handles, &teams)
+                    .join()
+                    .collect::<Vec<_>>();
+
+                // Need to use `find()` because the joins may be presented out of order.
+                assert_eq!(2, components.len());
+                assert!(
+                    components
+                        .iter()
+                        .find(|(&input_controlled, _character_prefab_handle, &team)| {
+                            input_controlled == InputControlled::new(0)
+                                && team == Team::Independent(IndependentCounter::new(0))
+                        })
+                        .is_some(),
+                    "Expected entity with `InputControlled`, `CharacterPrefabHandle`, and \
+                     `Team` components to exist."
+                );
+                assert!(
+                    components
+                        .iter()
+                        .find(|(&input_controlled, _character_prefab_handle, &team)| {
+                            input_controlled == InputControlled::new(123)
+                                && team == Team::Independent(IndependentCounter::new(1))
+                        })
+                        .is_some(),
+                    "Expected entity with `InputControlled`, `CharacterPrefabHandle`, and \
+                     `Team` components to exist."
+                );
+
+                assert_eq!(
+                    2,
+                    world
+                        .read_resource::<GameEntities>()
+                        .objects
+                        .get(&ObjectType::Character)
+                        .expect("Expected `ObjectType::Character` key in `GameEntities`.")
+                        .len()
+                );
                 assert_eq!(
                     CharacterAugmentStatus::Rectify,
                     world
                         .read_resource::<GameLoadingStatus>()
                         .character_augment_status
                 );
+            },
+        )
+    }
+
+    fn run_test(setup_fn: fn(&mut World), assertion_fn: fn(&mut World)) -> Result<(), Error> {
+        env::set_var("APP_DIR", env!("CARGO_MANIFEST_DIR"));
+
+        AmethystApplication::blank()
+            .with_bundle(TransformBundle::new())
+            .with_bundle(RenderEmptyBundle::<DefaultBackend>::new())
+            .with_custom_event_type::<AppEvent, AppEventReader>()
+            .with_resource(ScreenDimensions::new(SCREEN_WIDTH, SCREEN_HEIGHT, HIDPI))
+            .with_ui_bundles::<ControlBindings>()
+            .with_system(Processor::<Source>::new(), "source_processor", &[])
+            .with_bundle(SpriteLoadingBundle::new())
+            .with_bundle(SequenceLoadingBundle::new())
+            .with_bundle(LoadingBundle::new(ASSETS_PATH.clone()))
+            .with_bundle(CollisionLoadingBundle::new())
+            .with_bundle(SpawnLoadingBundle::new())
+            .with_bundle(MapLoadingBundle::new())
+            .with_bundle(CharacterLoadingBundle::new())
+            .with_bundle(
+                CharacterPrefabBundle::new()
+                    .with_system_dependencies(&[String::from(CHARACTER_PROCESSOR)]),
+            )
+            .with_bundle(CollisionAudioLoadingBundle::new(ASSETS_PATH.clone()))
+            .with_bundle(UiAudioLoadingBundle::new(ASSETS_PATH.clone()))
+            .with_state(|| LoadingState::new(PopState))
+            .with_setup(|world| {
+                <CharacterSelectionSpawningSystem as System>::SystemData::setup(&mut world.res)
             })
+            .with_setup(setup_fn)
+            .with_system_single(
+                CharacterSelectionSpawningSystem,
+                CharacterSelectionSpawningSystem::type_name(),
+                &[],
+            ) // kcov-ignore
+            .with_assertion(assertion_fn)
             .run_isolated()
     }
+
+    type TestSystemData<'s> = (
+        ReadStorage<'s, InputControlled>,
+        ReadStorage<'s, CharacterPrefabHandle>,
+        ReadStorage<'s, Team>,
+    );
 }
---
+++ crate/team_model/src/play/independent_counter.rs
@@ -0,0 +1,42 @@
+use derive_more::{Add, AddAssign, Display, From, Sub, SubAssign};
+use numeric_newtype_derive::numeric_newtype;
+use serde::{Deserialize, Serialize};
+
+/// Newtype for counting independent team number.
+///
+/// The value this holds should be valid when retrieved. If the value is assigned to a new
+/// `Independent` team, then it must be incremented after. The `#get_and_increment` method is
+/// provided to make it easier to uphold this contract.
+#[numeric_newtype]
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct IndependentCounter(pub u32);
+
+impl IndependentCounter {
+    /// Returns a copy of the current counter value, and increments itself.
+    pub fn get_and_increment(&mut self) -> Self {
+        let current_value = *self;
+        self.0 += 1;
+        current_value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::IndependentCounter;
+
+    #[test]
+    fn get_and_increment_returns_incremented_value() {
+        let mut independent_counter = IndependentCounter::new(5);
+        let get = independent_counter.get_and_increment();
+
+        assert_eq!(IndependentCounter::new(5), get);
+    }
+
+    #[test]
+    fn get_and_increment_increments_self() {
+        let mut independent_counter = IndependentCounter::new(5);
+        let _ = independent_counter.get_and_increment();
+
+        assert_eq!(IndependentCounter::new(6), independent_counter);
+    }
+}
