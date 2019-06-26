0126a2a993634ec5e8e9620438a10143b449b0e2
Azriel Hoh <mail@azriel.im>
2019-06-26 15:46:29 +1200

    Implemented `ContactDetectionSystem`.
    Issue #140

--- crate/collision_model/src/play.rs
+++ crate/collision_model/src/play.rs
@@ -1,12 +1,13 @@
 //! Types used during game play.
 
 pub use self::{
-    collision_event::CollisionEvent, hit_event::HitEvent, hit_object_count::HitObjectCount,
-    hit_repeat_clock::HitRepeatClock, hit_repeat_tracker::HitRepeatTracker,
-    hit_repeat_trackers::HitRepeatTrackers,
+    collision_event::CollisionEvent, contact_event::ContactEvent, hit_event::HitEvent,
+    hit_object_count::HitObjectCount, hit_repeat_clock::HitRepeatClock,
+    hit_repeat_tracker::HitRepeatTracker, hit_repeat_trackers::HitRepeatTrackers,
 };
 
 mod collision_event;
+mod contact_event;
 mod hit_event;
 mod hit_object_count;
 mod hit_repeat_clock;
