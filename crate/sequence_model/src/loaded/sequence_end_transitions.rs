5dffa35edbecbc2a65194b080d83ff038d600f57
Azriel Hoh <mail@azriel.im>
2019-07-11 14:21:08 +1200

    Implement `Component` for `SequenceEndTransitions`.
    This will likely go away when the `SequenceComponentUpdateSystem` is
    completed.
    
    Issue #147

--- crate/sequence_model/src/loaded/sequence_end_transitions.rs
+++ crate/sequence_model/src/loaded/sequence_end_transitions.rs
@@ -1,9 +1,12 @@
+use amethyst::ecs::{storage::DenseVecStorage, Component};
 use sequence_model_derive::sequence_component_data;
+use specs_derive::Component;
 
 use crate::config::{SequenceEndTransition, SequenceId};
 
 /// Sequence of sequence transitions upon sequence end.
 #[sequence_component_data(SeqId, SequenceEndTransition<SeqId>)]
+#[derive(Component)]
 pub struct SequenceEndTransitions<SeqId>
 where
     SeqId: SequenceId;
