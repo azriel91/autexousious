a77ddecb343b213293fac4ba8627f93f75051f5e
Azriel Hoh <mail@azriel.im>
2019-07-29 13:40:11 +1200

    Removed unused sequence handler code.
    These have been replaced by default `control_input_transitions.toml`.
    
    Issue #115

--- crate/character_play/src/sequence_handler/common/input.rs
+++ crate/character_play/src/sequence_handler/common/input.rs
@@ -1,13 +1,9 @@
 pub(crate) use self::{
-    dash_back_check::DashBackCheck, dash_forward_check::DashForwardCheck, dodge_check::DodgeCheck,
     run_stop_check::RunStopCheck, stand_x_movement_check::StandXMovementCheck,
     stand_z_movement_check::StandZMovementCheck, walk_no_movement_check::WalkNoMovementCheck,
     walk_x_movement_check::WalkXMovementCheck, walk_z_movement_check::WalkZMovementCheck,
 };
 
-mod dash_back_check;
-mod dash_forward_check;
-mod dodge_check;
 mod run_stop_check;
 mod stand_x_movement_check;
 mod stand_z_movement_check;
