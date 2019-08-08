fd9d0b9f354c13d8b3cadb6afffef5a923cf485f
Azriel Hoh <mail@azriel.im>
2019-08-08 17:06:25 +1200

    Implemented `ChargeIncrementSystem`.
    Issue #148

--- crate/charge_play/src/system.rs
+++ crate/charge_play/src/system.rs
@@ -1,7 +1,9 @@
 pub use self::{
+    charge_increment_system::ChargeIncrementSystem,
     charge_initialize_delay_system::ChargeInitializeDelaySystem,
     charge_initialize_detection_system::ChargeInitializeDetectionSystem,
 };
 
+mod charge_increment_system;
 mod charge_initialize_delay_system;
 mod charge_initialize_detection_system;
