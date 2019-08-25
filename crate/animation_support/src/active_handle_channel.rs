325b1e7f9a49f77e32ee5e9c2620e475c2d14b41
Azriel Hoh <mail@azriel.im>
2019-08-25 15:07:00 +1200

    Removed `animation_support` crate.
    Issue #155

--- crate/animation_support/src/active_handle_channel.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-/// Channels that are animatable on `ActiveHandle`
-#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
-pub enum ActiveHandleChannel {
-    /// The handle to use.
-    Handle,
-}
