b292bb52296d73c21b0bbaa45e3a7cb09dff6c23
Azriel Hoh <mail@azriel.im>
2019-08-29 11:11:38 +1200

    Generate kinematic types using macro.
    Issue #125

--- crate/kinematic_model/src/config.rs
+++ crate/kinematic_model/src/config.rs
@@ -1,6 +1,198 @@
 //! Contains the types that represent the configuration on disk.
 
-pub use self::{position::Position, velocity::Velocity};
+use std::{
+    fmt::Debug,
+    ops::{Deref, DerefMut},
+};
 
-mod position;
-mod velocity;
+use amethyst::{
+    core::math::Vector3,
+    ecs::{storage::DenseVecStorage, Component},
+};
+use serde::{Deserialize, Serialize};
+use specs_derive::Component;
+
+macro_rules! kinematic_type {
+    ($name:ident) => {
+        /// #[doc = $name]
+        /// of the entity in game.
+        #[derive(Clone, Component, Copy, Debug, Deserialize, PartialEq, Eq, Serialize)]
+        pub struct $name<S>(pub Vector3<S>)
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static;
+
+        impl<S> $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            /// Returns a new `
+            /// #[doc = $name]
+            /// `.
+            pub fn new(x: S, y: S, z: S) -> Self {
+                $name(Vector3::new(x, y, z))
+            }
+        }
+
+        impl<S> Default for $name<S>
+        where
+            S: Clone + Copy + Debug + Default + PartialEq + Send + Sync + 'static,
+        {
+            fn default() -> Self {
+                $name(Vector3::new(S::default(), S::default(), S::default()))
+            }
+        }
+
+        impl<S> From<Vector3<S>> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: Vector3<S>) -> Self {
+                $name(v)
+            }
+        }
+
+        impl<'a, S> From<&'a Vector3<S>> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: &'a Vector3<S>) -> Self {
+                $name(*v)
+            }
+        }
+
+        impl<'a, S> From<&'a mut Vector3<S>> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: &'a mut Vector3<S>) -> Self {
+                $name(*v)
+            }
+        }
+
+        impl<S> From<(S, S, S)> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: (S, S, S)) -> Self {
+                $name::new(v.0, v.1, v.2)
+            }
+        }
+
+        impl<'a, S> From<&'a (S, S, S)> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: &'a (S, S, S)) -> Self {
+                $name::new(v.0, v.1, v.2)
+            }
+        }
+
+        impl<'a, S> From<&'a mut (S, S, S)> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: &'a mut (S, S, S)) -> Self {
+                $name::new(v.0, v.1, v.2)
+            }
+        }
+
+        impl<S> From<[S; 3]> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: [S; 3]) -> Self {
+                $name::new(v[0], v[1], v[2])
+            }
+        }
+
+        impl<'a, S> From<&'a [S; 3]> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: &'a [S; 3]) -> Self {
+                $name::new(v[0], v[1], v[2])
+            }
+        }
+
+        impl<'a, S> From<&'a mut [S; 3]> for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn from(v: &'a mut [S; 3]) -> Self {
+                $name::new(v[0], v[1], v[2])
+            }
+        }
+
+        impl<S> Deref for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            type Target = Vector3<S>;
+
+            fn deref(&self) -> &Vector3<S> {
+                &self.0
+            }
+        }
+
+        impl<S> DerefMut for $name<S>
+        where
+            S: Clone + Copy + Debug + PartialEq + Send + Sync + 'static,
+        {
+            fn deref_mut(&mut self) -> &mut Vector3<S> {
+                &mut self.0
+            }
+        }
+    };
+}
+
+kinematic_type!(Position);
+kinematic_type!(Velocity);
+
+#[cfg(test)]
+mod tests {
+    use amethyst::core::math::Vector3;
+
+    use super::Position;
+
+    #[test]
+    fn from_vector3() {
+        assert_eq!(
+            Position::new(1., 2.1, 1.5),
+            Vector3::new(1., 2.1, 1.5).into()
+        );
+        assert_eq!(
+            Position::new(1., 2.1, 1.5),
+            (&Vector3::new(1., 2.1, 1.5)).into()
+        );
+        assert_eq!(
+            Position::new(1., 2.1, 1.5),
+            (&mut Vector3::new(1., 2.1, 1.5)).into()
+        );
+    }
+
+    #[test]
+    fn from_tuple() {
+        assert_eq!(Position::new(1., 2.1, 1.5), (1., 2.1, 1.5).into());
+        assert_eq!(Position::new(1., 2.1, 1.5), (&(1., 2.1, 1.5)).into());
+        assert_eq!(Position::new(1., 2.1, 1.5), (&mut (1., 2.1, 1.5)).into());
+    }
+
+    #[test]
+    fn from_slice() {
+        assert_eq!(Position::new(1., 2.1, 1.5), [1., 2.1, 1.5].into());
+        assert_eq!(Position::new(1., 2.1, 1.5), (&[1., 2.1, 1.5]).into());
+        assert_eq!(Position::new(1., 2.1, 1.5), (&mut [1., 2.1, 1.5]).into());
+    }
+
+    #[test]
+    fn deref() {
+        assert_eq!(Vector3::new(1., 2., 3.), *Position::new(1., 2., 3.));
+    }
+
+    #[test]
+    fn deref_mut() {
+        let mut velocity = Position::default();
+        *velocity += Vector3::new(1., 2., 3.);
+        assert_eq!(Vector3::new(1., 2., 3.), *velocity);
+    }
+}
