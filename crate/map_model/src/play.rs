c10fca450d53c79e12a2ac5da88467a4b25a58a1
Azriel Hoh <mail@azriel.im>
2019-09-02 09:39:51 +1200

    Implemented `KeepWithinMapBoundsSystem`.
    Issue #142

--- crate/map_model/src/play.rs
+++ crate/map_model/src/play.rs
@@ -2,9 +2,10 @@
 
 pub use self::{
     boundary_face::BoundaryFace, map_boundary_event::MapBoundaryEvent,
-    map_boundary_event_data::MapBoundaryEventData,
+    map_boundary_event_data::MapBoundaryEventData, map_bounded::MapBounded,
 };
 
 mod boundary_face;
 mod map_boundary_event;
 mod map_boundary_event_data;
+mod map_bounded;
--- crate/map_play/src/system.rs
+++ crate/map_play/src/system.rs
@@ -1,3 +1,7 @@
-pub use self::map_enter_exit_detection_system::MapEnterExitDetectionSystem;
+pub use self::{
+    keep_within_map_bounds_system::KeepWithinMapBoundsSystem,
+    map_enter_exit_detection_system::MapEnterExitDetectionSystem,
+};
 
+mod keep_within_map_bounds_system;
 mod map_enter_exit_detection_system;
