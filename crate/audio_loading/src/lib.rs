d485506f771153b29befa3815e3bfd19577b30e5
Azriel Hoh <mail@azriel.im>
2019-09-10 10:46:59 +1200

    Added `audio_loading` crate.
    Issue #163

---
+++ crate/audio_loading/src/lib.rs
@@ -0,0 +1,7 @@
+#![deny(missing_debug_implementations, missing_docs)] // kcov-ignore
+
+//! Processes audio configuration into the loaded audio model.
+
+pub use crate::audio_loading_bundle::AudioLoadingBundle;
+
+mod audio_loading_bundle;
