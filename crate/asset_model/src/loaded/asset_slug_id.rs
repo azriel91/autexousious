66deb9ce0bffc43ed22ea8afec34140b979aa819
Azriel Hoh <mail@azriel.im>
2019-09-17 11:18:59 +1200

    Use `slotmap` to define `AssetSlugId` type.
    Issue #167

--- crate/asset_model/src/loaded/asset_slug_id.rs
+++ crate/asset_model/src/loaded/asset_slug_id.rs
@@ -1,13 +1,12 @@
 use amethyst::ecs::{storage::VecStorage, Component};
-use derive_more::{Add, AddAssign, Display, From, Sub, SubAssign};
-use numeric_newtype_derive::numeric_newtype;
-use serde::{Deserialize, Serialize};
-use specs_derive::Component;
 
-/// Asset slug ID.
-///
-/// This is a cheap `Copy` type to use instead of `AssetSlug` which is `Clone`.
-#[numeric_newtype]
-#[derive(Component, Debug, Deserialize, Hash, Serialize)]
-#[storage(VecStorage)]
-pub struct AssetSlugId(pub usize);
+slotmap::new_key_type! {
+    /// Asset slug ID.
+    ///
+    /// This is a cheap `Copy` type to use instead of `AssetSlug` which is `Clone`.
+    pub struct AssetSlugId;
+}
+
+impl Component for AssetSlugId {
+    type Storage = VecStorage<Self>;
+}
