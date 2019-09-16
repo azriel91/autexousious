799032504c9070f0c3226408433bc104ee40f1f3
Azriel Hoh <mail@azriel.im>
2019-09-17 08:16:05 +1200

    Implemented `AssetSlugTypeMappings`.
    Issue #167

--- crate/asset_model/src/loaded.rs
+++ crate/asset_model/src/loaded.rs
@@ -2,9 +2,10 @@
 
 pub use self::{
     asset_slug_id::AssetSlugId, asset_slug_id_mappings::AssetSlugIdMappings,
-    slug_and_handle::SlugAndHandle,
+    asset_slug_type_mappings::AssetSlugTypeMappings, slug_and_handle::SlugAndHandle,
 };
 
 mod asset_slug_id;
 mod asset_slug_id_mappings;
+mod asset_slug_type_mappings;
 mod slug_and_handle;
