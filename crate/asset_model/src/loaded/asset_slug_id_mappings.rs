42397fa0f8f2fab7da7d9ed3c41e6887a6006a5b
Azriel Hoh <mail@azriel.im>
2019-09-17 13:52:19 +1200

    Updated `AssetSlugIdMappings` implementation to back onto slotmap types.
    Issue #167

--- crate/asset_model/src/loaded/asset_slug_id_mappings.rs
+++ crate/asset_model/src/loaded/asset_slug_id_mappings.rs
@@ -1,19 +1,18 @@
-use std::iter::FromIterator;
+use std::{collections::HashMap, iter::FromIterator};
 
-use bimap::BiMap;
-use derive_deref::{Deref, DerefMut};
-use derive_new::new;
+use slotmap::DenseSlotMap;
 
 use crate::{config::AssetSlug, loaded::AssetSlugId};
 
 /// Mappings from asset slug to ID, and ID to slug.
 ///
-/// This is essentially a wrapper around `BiMap`, with the `slug()` and `id()` methods.
-#[derive(Clone, Debug, Default, Deref, DerefMut, PartialEq, new)]
+/// Asset slugs are intended to be inserted / removed, but not modified.
+#[derive(Clone, Debug, Default)]
 pub struct AssetSlugIdMappings {
-    /// Bi-directional mapping from asset slug to id.
-    #[new(default)]
-    pub asset_slug_to_id: BiMap<AssetSlug, AssetSlugId>,
+    /// Mapping from asset ID to slug.
+    asset_id_to_slug: DenseSlotMap<AssetSlugId, AssetSlug>,
+    /// Mapping from asset slug to id.
+    asset_slug_to_id: HashMap<AssetSlug, AssetSlugId>,
 }
 
 impl AssetSlugIdMappings {
@@ -22,24 +21,100 @@ impl AssetSlugIdMappings {
     /// The mappings are guaranteed to hold `capacity` elements without re-allocating.
     pub fn with_capacity(capacity: usize) -> Self {
         AssetSlugIdMappings {
-            asset_slug_to_id: BiMap::with_capacity(capacity),
+            asset_id_to_slug: DenseSlotMap::with_capacity_and_key(capacity),
+            asset_slug_to_id: HashMap::with_capacity(capacity),
         }
     }
 
     /// Returns the asset slug for the given ID.
     pub fn slug(&self, asset_slug_id: AssetSlugId) -> Option<&AssetSlug> {
-        self.asset_slug_to_id.get_by_right(&asset_slug_id)
+        self.asset_id_to_slug.get(asset_slug_id)
     }
 
     /// Returns the asset slug ID for the given asset slug.
     pub fn id(&self, asset_slug: &AssetSlug) -> Option<&AssetSlugId> {
-        self.asset_slug_to_id.get_by_left(asset_slug)
+        self.asset_slug_to_id.get(asset_slug)
+    }
+
+    /// Returns `true` if there are no mappings.
+    pub fn is_empty(&self) -> bool {
+        self.asset_slug_to_id.is_empty()
+    }
+
+    /// Inserts an asset slug into the mappings and returns the generated ID.
+    ///
+    /// # Parameters
+    ///
+    /// * `asset_slug`: Asset slug to insert.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the number of mappings equals 2<sup>32</sup> - 2.
+    pub fn insert(&mut self, asset_slug: AssetSlug) -> AssetSlugId {
+        let asset_slug_id = self.asset_id_to_slug.insert(asset_slug.clone());
+        self.asset_slug_to_id.insert(asset_slug, asset_slug_id);
+
+        asset_slug_id
+    }
+
+    /// Returns an iterator of asset slug IDs to slug.
+    pub fn iter(&self) -> impl Iterator<Item = (AssetSlugId, &AssetSlug)> {
+        self.asset_id_to_slug.iter()
+    }
+
+    /// Returns an iterator visiting all `AssetSlugId`s in arbitrary order.
+    pub fn keys<'a>(&'a self) -> impl Iterator<Item = AssetSlugId> + 'a {
+        self.asset_id_to_slug.keys()
+    }
+
+    /// Returns an iterator visiting all `AssetSlug`s in arbitrary order.
+    pub fn values(&self) -> impl Iterator<Item = &AssetSlug> {
+        self.asset_id_to_slug.values()
+    }
+
+    /// Returns the number of mappings.
+    pub fn len(&self) -> usize {
+        self.asset_id_to_slug.len()
+    }
+
+    /// Removes the ID mapping for the given asset slug, returning it if it exists.
+    pub fn remove(&mut self, asset_slug_id: AssetSlugId) -> Option<AssetSlug> {
+        let asset_slug = self.asset_id_to_slug.remove(asset_slug_id);
+        if let Some(asset_slug) = asset_slug.as_ref() {
+            self.asset_slug_to_id.remove(asset_slug);
+        }
+
+        asset_slug
+    }
+
+    /// Reserves capacity for at least `additional` more mappings to be inserted.
+    ///
+    /// This may reserve more space to avoid frequent reallocations.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the new allocation size overflows `usize`.
+    pub fn reserve(&mut self, additional: usize) {
+        self.asset_id_to_slug.reserve(additional);
+        self.asset_slug_to_id.reserve(additional);
     }
 }
 
-impl FromIterator<(AssetSlug, AssetSlugId)> for AssetSlugIdMappings {
-    fn from_iter<T: IntoIterator<Item = (AssetSlug, AssetSlugId)>>(iter: T) -> AssetSlugIdMappings {
-        let asset_slug_to_id = BiMap::from_iter(iter);
-        AssetSlugIdMappings { asset_slug_to_id }
+impl FromIterator<AssetSlug> for AssetSlugIdMappings {
+    fn from_iter<T: IntoIterator<Item = AssetSlug>>(iter: T) -> AssetSlugIdMappings {
+        let (asset_id_to_slug, asset_slug_to_id) = iter.into_iter().fold(
+            (DenseSlotMap::with_key(), HashMap::new()),
+            |(mut asset_id_to_slug, mut asset_slug_to_id), asset_slug| {
+                let asset_slug_id = asset_id_to_slug.insert(asset_slug.clone());
+                asset_slug_to_id.insert(asset_slug, asset_slug_id);
+
+                (asset_id_to_slug, asset_slug_to_id)
+            },
+        );
+
+        AssetSlugIdMappings {
+            asset_id_to_slug,
+            asset_slug_to_id,
+        }
     }
 }
