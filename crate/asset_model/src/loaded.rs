1c9286b5244924231c52aebac9c15e465c31f4d6
Azriel Hoh <mail@azriel.im>
2019-09-17 14:03:06 +1200

    Renamed `AssetSlugTypeMappings` to `AssetTypeMappings`.
    Issue #167

--- crate/asset_model/src/loaded.rs
+++ crate/asset_model/src/loaded.rs
@@ -2,10 +2,10 @@
 
 pub use self::{
     asset_slug_id::AssetSlugId, asset_slug_id_mappings::AssetSlugIdMappings,
-    asset_slug_type_mappings::AssetSlugTypeMappings, slug_and_handle::SlugAndHandle,
+    asset_type_mappings::AssetTypeMappings, slug_and_handle::SlugAndHandle,
 };
 
 mod asset_slug_id;
 mod asset_slug_id_mappings;
-mod asset_slug_type_mappings;
+mod asset_type_mappings;
 mod slug_and_handle;
---
+++ crate/asset_model/src/loaded/asset_type_mappings.rs
@@ -0,0 +1,45 @@
+use std::iter::FromIterator;
+
+use bimap::BiMap;
+use derive_deref::{Deref, DerefMut};
+use derive_new::new;
+
+use crate::{config::AssetType, loaded::AssetSlugId};
+
+/// Mappings from asset ID to asset type, and asset type to ID.
+///
+/// This is essentially a wrapper around `BiMap`, with the `asset_id()` and `asset_type()` methods.
+#[derive(Clone, Debug, Default, Deref, DerefMut, PartialEq, new)]
+pub struct AssetTypeMappings {
+    /// Bi-directional mapping from asset ID to asset type.
+    #[new(default)]
+    pub asset_id_to_type: BiMap<AssetSlugId, AssetType>,
+}
+
+impl AssetTypeMappings {
+    /// Returns a `AssetTypeMappings` with pre-allocated capacity.
+    ///
+    /// The mappings are guaranteed to hold `capacity` elements without re-allocating.
+    pub fn with_capacity(capacity: usize) -> Self {
+        AssetTypeMappings {
+            asset_id_to_type: BiMap::with_capacity(capacity),
+        }
+    }
+
+    /// Returns the asset ID for the given asset type.
+    pub fn asset_id(&self, asset_type: AssetType) -> Option<&AssetSlugId> {
+        self.asset_id_to_type.get_by_right(&asset_type)
+    }
+
+    /// Returns the asset type for the given asset ID.
+    pub fn asset_type(&self, asset_id: &AssetSlugId) -> Option<&AssetType> {
+        self.asset_id_to_type.get_by_left(asset_id)
+    }
+}
+
+impl FromIterator<(AssetSlugId, AssetType)> for AssetTypeMappings {
+    fn from_iter<T: IntoIterator<Item = (AssetSlugId, AssetType)>>(iter: T) -> AssetTypeMappings {
+        let asset_id_to_type = BiMap::from_iter(iter);
+        AssetTypeMappings { asset_id_to_type }
+    }
+}
