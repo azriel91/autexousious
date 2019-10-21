d36a21f6c107749d8bc0952acd229ffb0c868ef2
Azriel Hoh <mail@azriel.im>
2019-10-21 19:11:26 +1300

    Added tests for `AssetSpritesDefinitionLoader`.
    Issue #172

--- crate/workspace_tests/src/loading/system.rs
+++ crate/workspace_tests/src/loading/system.rs
@@ -3,3 +3,5 @@ mod asset_discovery_system;
 mod asset_id_mapping_system;
 mod asset_part_loading_coordinator_system;
 mod asset_part_loading_system;
+// mod asset_sequence_component_loading_system; // TODO: refactor first
+mod asset_sprites_definition_loading_system;
