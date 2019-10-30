a6a437b864642a3ca463f26952a2bf36074a2ead
Azriel Hoh <mail@azriel.im>
2019-10-30 15:52:44 +1300

    Added `SpriteFrame` to `sprite_model` crate.
    Issue #175

--- crate/sprite_model/src/config.rs
+++ crate/sprite_model/src/config.rs
@@ -1,10 +1,11 @@
 //! User defined configuration types for sprites.
 
 pub use self::{
-    sprite_offset::SpriteOffset, sprite_ref::SpriteRef,
+    sprite_frame::SpriteFrame, sprite_offset::SpriteOffset, sprite_ref::SpriteRef,
     sprite_sheet_definition::SpriteSheetDefinition, sprites_definition::SpritesDefinition,
 };
 
+mod sprite_frame;
 mod sprite_offset;
 mod sprite_ref;
 mod sprite_sheet_definition;
