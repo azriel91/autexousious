baf5267b98718978f1f53cb2858e7e5a1c0f2f00
Azriel Hoh <mail@azriel.im>
2019-11-08 16:14:33 +1300

    Implemented `MapSpawnOutOfBoundsDetectionSystem`.
    Issue #184

--- crate/workspace_tests/src/map_play/system.rs
+++ crate/workspace_tests/src/map_play/system.rs
@@ -2,3 +2,4 @@ mod keep_within_map_bounds_system;
 mod map_enter_exit_detection_system;
 mod map_out_of_bounds_clock_augment_system;
 mod map_out_of_bounds_deletion_system;
+mod map_spawn_out_of_bounds_detection_system;
