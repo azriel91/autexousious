abe04c3c9d88757dede54c9c30fb24e735f2cf4e
Azriel Hoh <mail@azriel.im>
2019-11-16 19:21:04 +1300

    Added `ui_button_model` crate.
    Issue #178

--- crate/workspace_tests/src/ui_model/config.rs
+++ crate/workspace_tests/src/ui_model/config.rs
@@ -8,6 +8,7 @@ mod test {
     use sequence_model::config::{SequenceEndTransition, SequenceNameString, Wait};
     use serde_yaml;
     use sprite_model::config::{SpriteFrame, SpriteRef};
+    use ui_button_model::config::{UiButton, UiButtons};
     use ui_label_model::config::{UiLabel, UiSpriteLabel};
     use ui_menu_item_model::config::{UiMenuItem, UiMenuItems};
 
@@ -26,6 +27,11 @@ menu:
     position: { x: -1, y: -2, z: -3 }
     sprite: { sequence: "exit_inactive" }
 
+buttons:
+  - position: { x: -4, y: -5, z: -6 }
+    label: { position: { x: -7, y: -8, z: -9 }, text: "Button Zero" }
+    sprite: { position: { x: -10, y: -11, z: -12 }, sequence: "button_inactive" }
+
 sequences:
   start_game_inactive:
     next: "none"
@@ -38,6 +44,11 @@ sequences:
       - { wait: 2, sprite: { sheet: 0, index: 0 } }
 
   exit_inactive:
+    next: "none"
+    frames:
+      - { wait: 2, sprite: { sheet: 0, index: 0 } }
+
+  button_inactive:
     next: "none"
     frames:
       - { wait: 2, sprite: { sheet: 0, index: 0 } }
@@ -71,6 +82,14 @@ sequences:
                 GameModeIndex::Exit,
             ),
         ]));
+        let buttons = UiButtons::new(vec![UiButton::new(
+            PositionInit::new(-4, -5, -6),
+            UiLabel::new(PositionInit::new(-7, -8, -9), String::from("Button Zero")),
+            UiSpriteLabel::new(
+                PositionInit::new(-10, -11, -12),
+                SequenceNameString::String(String::from("button_inactive")),
+            ),
+        )]);
         let sequences = {
             let mut sequences = IndexMap::new();
             sequences.insert(
@@ -85,9 +104,17 @@ sequences:
                 SequenceNameString::String(String::from("exit_inactive")),
                 UiSequence::new(SequenceEndTransition::None, sprite_frames()),
             );
+            sequences.insert(
+                SequenceNameString::String(String::from("button_inactive")),
+                UiSequence::new(SequenceEndTransition::None, sprite_frames()),
+            );
             UiSequences::new(sequences)
         };
-        let ui_definition_expected = UiDefinition { ui_type, sequences };
+        let ui_definition_expected = UiDefinition {
+            ui_type,
+            buttons,
+            sequences,
+        };
 
         assert_eq!(ui_definition_expected, ui_definition);
     }
