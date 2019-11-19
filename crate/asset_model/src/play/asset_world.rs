2dea2f144b6f9fb3a3119746f2100637f893144f
Azriel Hoh <mail@azriel.im>
2019-11-20 08:40:07 +1300

    Fixed `AssetWorld` not being set up.
    Issue #186

--- crate/asset_model/src/play/asset_world.rs
+++ crate/asset_model/src/play/asset_world.rs
@@ -1,6 +1,16 @@
-pub use amethyst::ecs::World;
+use amethyst::ecs::{World, WorldExt};
+use derivative::Derivative;
+use derive_deref::{Deref, DerefMut};
 
 /// `World` for assets.
 ///
 /// Type alias simply makes it clearer that we should use a separate `World` instance.
-pub type AssetWorld = World;
+#[derive(Derivative, Deref, DerefMut)]
+#[derivative(Debug)]
+pub struct AssetWorld(#[derivative(Debug = "ignore")] pub World);
+
+impl Default for AssetWorld {
+    fn default() -> Self {
+        AssetWorld(World::new())
+    }
+}
