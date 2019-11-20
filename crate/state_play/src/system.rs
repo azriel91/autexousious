d0d0165d276e8ba3dadd08b9f5e1ea52bbd1853f
Azriel Hoh <mail@azriel.im>
2019-11-21 09:52:54 +1300

    Implemented `StateItemUiInputAugmentSystem`.
    Issue #186

--- crate/state_play/src/system.rs
+++ crate/state_play/src/system.rs
@@ -2,6 +2,9 @@ pub use self::{
     state_camera_reset_system::{StateCameraResetSystem, StateCameraResetSystemData},
     state_id_event_system::{StateIdEventSystem, StateIdEventSystemData},
     state_item_spawn_system::{StateItemSpawnSystem, StateItemSpawnSystemData},
+    state_item_ui_input_augment_system::{
+        StateItemUiInputAugmentSystem, StateItemUiInputAugmentSystemData,
+    },
     state_item_ui_rectify_system::{StateItemUiRectifySystem, StateItemUiRectifySystemData},
     state_ui_spawn_system::{StateUiSpawnSystem, StateUiSpawnSystemData},
 };
@@ -9,5 +12,6 @@ pub use self::{
 mod state_camera_reset_system;
 mod state_id_event_system;
 mod state_item_spawn_system;
+mod state_item_ui_input_augment_system;
 mod state_item_ui_rectify_system;
 mod state_ui_spawn_system;
