d7934c24087f4b0ee2a3ca90d31bdeed28197d7f
Azriel Hoh <mail@azriel.im>
2019-11-25 16:01:29 +1300

    Insert `SequenceStatus` when `SequenceId` is an item component.
    Issue #186

--- crate/sequence_model/src/loaded/sequence_id.rs
+++ crate/sequence_model/src/loaded/sequence_id.rs
@@ -12,6 +12,8 @@ use numeric_newtype_derive::numeric_newtype;
 use serde::{Deserialize, Serialize};
 use typename_derive::TypeName;
 
+use crate::play::SequenceStatus;
+
 /// Sequence ID of an object.
 #[numeric_newtype]
 #[derive(Debug, Default, Deserialize, Hash, Serialize, TypeName)]
@@ -30,18 +32,29 @@ pub struct SequenceIdSystemData<'s> {
     /// `SequenceId` components.
     #[derivative(Debug = "ignore")]
     pub sequence_ids: WriteStorage<'s, SequenceId>,
+    /// `SequenceStatus` components.
+    #[derivative(Debug = "ignore")]
+    pub sequence_statuses: WriteStorage<'s, SequenceStatus>,
 }
 
 impl<'s> ItemComponent<'s> for SequenceId {
     type SystemData = SequenceIdSystemData<'s>;
 
     fn augment(&self, system_data: &mut Self::SystemData, entity: Entity) {
-        let SequenceIdSystemData { sequence_ids } = system_data;
+        let SequenceIdSystemData {
+            sequence_ids,
+            sequence_statuses,
+        } = system_data;
 
         if sequence_ids.get(entity).is_none() {
             sequence_ids
                 .insert(entity, *self)
                 .expect("Failed to insert `SequenceId` component.");
+        }
+        if sequence_statuses.get(entity).is_none() {
+            sequence_statuses
+                .insert(entity, SequenceStatus::default())
+                .expect("Failed to insert `SequenceStatus` component.");
         }
     }
 }
