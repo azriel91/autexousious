7272286b0452277ea662827a0356786fbd0b8b8b
Azriel Hoh <mail@azriel.im>
2019-11-29 11:28:14 +1300

    Removed `ui_label_loading` and `ui_menu_item_loading` crates.
    These are no longer used.
    
    Issue #186

--- crate/ui_label_loading/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![deny(missing_debug_implementations, missing_docs)] // kcov-ignore
-
-//! Loading logic for `UiMenuItem`s.
-
-pub use crate::ui_labels_loader::UiLabelsLoader;
-
-mod ui_labels_loader;
