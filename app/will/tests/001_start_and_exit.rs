56004e0fe4e7f0cdc6b21b3f78072f7baa6da304
Azriel Hoh <mail@azriel.im>
2020-01-08 10:46:27 +1300

    Updated tests to compile.
    Issue #189

--- app/will/tests/001_start_and_exit.rs
+++ app/will/tests/001_start_and_exit.rs
@@ -1,23 +1,19 @@
 #![cfg(not(windows))] // TODO: <https://gitlab.com/azriel91/autexousious/issues/100>
 
-use assert_cmd::{
-    assert::OutputAssertExt,
-    cmd::{OutputError, OutputOkExt},
-    stdin::CommandStdInExt,
-};
+use assert_cmd::{assert::OutputAssertExt, output::OutputError, Command};
 use escargot::CargoBuild;
 
 #[test]
 #[ignore] // Can't test on X through CI.
 fn start_and_exit() -> Result<(), OutputError> {
-    CargoBuild::new()
+    let command = CargoBuild::new()
         .bin("will")
         .current_release()
         .run()
         .expect("Failed to create `cargo` command")
-        .command()
-        .with_stdin()
-        .buffer("exit\n")
+        .command();
+    Command::from_std(command)
+        .write_stdin("exit\n")
         .ok()?
         .assert()
         .success();
