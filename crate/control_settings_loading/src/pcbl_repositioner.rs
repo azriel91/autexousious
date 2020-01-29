87bff3b9f7438441a901c3525afac684a1f841d6
Azriel Hoh <mail@azriel.im>
2020-01-29 13:10:34 +1300

    Remove half control buttons width shift.
    Issue #191

--- crate/control_settings_loading/src/pcbl_repositioner.rs
+++ crate/control_settings_loading/src/pcbl_repositioner.rs
@@ -69,73 +69,16 @@ impl PcblRepositioner {
         let midpoint_distance = camera_zoom_dimensions.width as i32
             / (player_control_buttons_labelses.len() + 1) as i32;
 
-        // Widths for each player's control button labels.
-        let control_button_labels_widths = player_control_buttons_labelses
-            .iter()
-            .map(Self::buttons_all_iter)
-            .map(Self::buttons_position_init_iter)
-            .map(Self::position_x_iter)
-            .map(|position_x_iter| {
-                // Need to get button width, and add it to the last button's coordinate.
-                // Width is calculated from the distance between the smallest two positions' x
-                // values.
-                let button_width = {
-                    let x_iter = position_x_iter.clone();
-                    let x_iter_2 = x_iter.clone();
-                    let x_min = x_iter.min();
-
-                    x_min.and_then(|x_min| x_iter_2.filter(|x| *x > x_min).min())
-                };
-
-                let x_max = position_x_iter.max();
-                let mut x_max = x_max.unwrap_or(0);
-                if let Some(button_width) = button_width {
-                    x_max += button_width;
-                }
-
-                x_max
-            })
-            .collect::<Vec<i32>>();
-
         player_control_buttons_labelses
             .iter_mut()
             .map(Self::buttons_all_iter_mut)
             .map(Self::buttons_position_init_iter_mut)
-            .zip(control_button_labels_widths.into_iter())
             .enumerate()
-            .for_each(
-                |(index, (buttons_position_init_iter, control_button_labels_width))| {
-                    let x_shift =
-                        (index + 1) as i32 * midpoint_distance - (control_button_labels_width << 1);
+            .for_each(|(index, buttons_position_init_iter)| {
+                let x_shift = (index + 1) as i32 * midpoint_distance;
 
-                    buttons_position_init_iter.for_each(|position_init| position_init.x += x_shift);
-                },
-            );
-    }
-
-    fn buttons_all_iter<'f>(
-        player_control_buttons_labels: &'f PlayerControlButtonsLabels,
-    ) -> impl Iterator<Item = &ControlButtonLabel> + Clone + 'f {
-        player_control_buttons_labels
-            .axes
-            .values()
-            .chain(player_control_buttons_labels.actions.values())
-    }
-
-    fn buttons_position_init_iter<'f>(
-        buttons_all_iter: impl Iterator<Item = &'f ControlButtonLabel> + Clone + 'f,
-    ) -> impl Iterator<Item = PositionInit> + Clone + 'f {
-        buttons_all_iter.map(Self::button_position)
-    }
-
-    fn button_position(control_button_label: &ControlButtonLabel) -> PositionInit {
-        control_button_label.sprite.position
-    }
-
-    fn position_x_iter<'f>(
-        position_init_iter: impl Iterator<Item = PositionInit> + Clone + 'f,
-    ) -> impl Iterator<Item = i32> + Clone + 'f {
-        position_init_iter.map(|position_init| position_init.x)
+                buttons_position_init_iter.for_each(|position_init| position_init.x += x_shift);
+            });
     }
 
     fn buttons_all_iter_mut<'f>(
