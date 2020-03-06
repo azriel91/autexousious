b4eaa9288a607cac51c65e0fc4c2c484f8acfe09
Azriel Hoh <mail@azriel.im>
2020-03-06 18:50:14 +1300

    Use derives instead of manual impls for `NetData`.
    Issue #204

--- crate/net_model/src/play/net_data.rs
+++ crate/net_model/src/play/net_data.rs
@@ -1,13 +1,10 @@
-use std::{
-    hash::{Hash, Hasher},
-    net::SocketAddr,
-};
+use std::net::SocketAddr;
 
 use derive_new::new;
 use serde::{Deserialize, Serialize};
 
 /// Data that came through a network connection.
-#[derive(Clone, Debug, Deserialize, Serialize, new)]
+#[derive(Clone, Debug, Deserialize, Hash, PartialEq, Eq, Serialize, new)]
 pub struct NetData<D> {
     /// `SocketAddr` of the sender of the data.
     ///
@@ -17,24 +14,3 @@ pub struct NetData<D> {
     /// The data.
     pub data: D,
 }
-
-impl<D> Hash for NetData<D>
-where
-    D: Hash,
-{
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.socket_addr.hash(state);
-        self.data.hash(state);
-    }
-}
-
-impl<D> PartialEq for NetData<D>
-where
-    D: PartialEq,
-{
-    fn eq(&self, other: &Self) -> bool {
-        self.socket_addr == other.socket_addr && self.data == other.data
-    }
-}
-
-impl<D> Eq for NetData<D> where D: Eq {}
