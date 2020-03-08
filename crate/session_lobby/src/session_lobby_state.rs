879743ab2e269f9ad175581aed34fede9310b203
Azriel Hoh <mail@azriel.im>
2020-03-08 16:43:30 +1300

    Switch to `CharacterSelectionState` on `SessionStartNotify` event.
    Issue #204

--- crate/session_lobby/src/session_lobby_state.rs
+++ crate/session_lobby/src/session_lobby_state.rs
@@ -1,9 +1,17 @@
+use std::any;
+
 use amethyst::{GameData, State, StateData, Trans};
 use application_event::AppEvent;
 use application_state::{AppState, AppStateBuilder};
+use character_selection::{
+    CharacterSelectionStateBuilder, CharacterSelectionStateDelegate, CharacterSelectionSystem,
+};
 use derivative::Derivative;
 use derive_new::new;
+use game_loading::GameLoadingState;
+use game_play::GamePlayState;
 use log::debug;
+use map_selection::{MapSelectionBundle, MapSelectionStateBuilder, MapSelectionStateDelegate};
 use session_lobby_model::{SessionLobbyEntity, SessionLobbyEvent};
 use state_registry::StateId;
 
@@ -34,6 +42,32 @@ impl SessionLobbyStateDelegate {
     fn initialize_state(data: StateData<'_, GameData<'static, 'static>>) {
         data.world.insert(StateId::SessionLobby);
     }
+
+    fn character_selection_state() -> Box<dyn State<GameData<'static, 'static>, AppEvent>> {
+        // kcov-ignore-start
+        let game_play_fn = || Box::new(GamePlayState::new());
+        let game_loading_fn = move || Box::new(GameLoadingState::new(game_play_fn));
+        let map_selection_fn = move || {
+            let state =
+                MapSelectionStateBuilder::new(MapSelectionStateDelegate::new(game_loading_fn))
+                    .with_bundle(MapSelectionBundle::new())
+                    .build();
+
+            Box::new(state)
+        };
+        // kcov-ignore-end
+        let state = CharacterSelectionStateBuilder::new(CharacterSelectionStateDelegate::new(
+            map_selection_fn,
+        ))
+        .with_system(
+            CharacterSelectionSystem::new(),
+            any::type_name::<CharacterSelectionSystem>(),
+            &[],
+        )
+        .build();
+
+        Box::new(state)
+    }
 }
 
 impl State<GameData<'static, 'static>, AppEvent> for SessionLobbyStateDelegate {
@@ -53,6 +87,10 @@ impl State<GameData<'static, 'static>, AppEvent> for SessionLobbyStateDelegate {
         if let AppEvent::SessionLobby(session_lobby_event) = event {
             debug!("Received session_lobby_event: {:?}", session_lobby_event);
             match session_lobby_event {
+                SessionLobbyEvent::SessionStartNotify => {
+                    let character_selection_state = Self::character_selection_state();
+                    Trans::Switch(character_selection_state)
+                }
                 SessionLobbyEvent::Back => Trans::Pop,
                 _ => Trans::None,
             }
