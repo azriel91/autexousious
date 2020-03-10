a421f95451a5e2a4bf96214e2ac409d4eed86752
Azriel Hoh <mail@azriel.im>
2020-03-11 11:16:35 +1300

    Don't send local `GameInputEvent`s if in online play.
    Issue #199

--- crate/game_input/src/system/input_to_game_input_system.rs
+++ crate/game_input/src/system/input_to_game_input_system.rs
@@ -10,6 +10,7 @@ use amethyst::{
 use derivative::Derivative;
 use derive_new::new;
 use game_input_model::{config::ControlBindings, GameInputEvent};
+use network_session_model::play::SessionStatus;
 
 /// Sends `GameInputEvent`s based on a subset of `InputEvent`s.
 #[derive(Debug, SystemDesc, new)]
@@ -23,6 +24,9 @@ pub struct InputToGameInputSystem {
 #[derive(Derivative, SystemData)]
 #[derivative(Debug)]
 pub struct InputToGameInputSystemData<'s> {
+    /// `SessionStatus` resource.
+    #[derivative(Debug = "ignore")]
+    pub session_status: Read<'s, SessionStatus>,
     /// `InputEvent<ControlBindings>` channel.
     #[derivative(Debug = "ignore")]
     pub input_ec: Read<'s, EventChannel<InputEvent<ControlBindings>>>,
@@ -37,13 +41,21 @@ impl<'s> System<'s> for InputToGameInputSystem {
     fn run(
         &mut self,
         InputToGameInputSystemData {
+            session_status,
             input_ec,
             mut game_input_ec,
         }: Self::SystemData,
     ) {
-        input_ec
-            .read(&mut self.input_event_rid)
-            .filter_map(|input_event| GameInputEvent::try_from(input_event).ok())
-            .for_each(|game_input_event| game_input_ec.single_write(game_input_event));
+        let input_events = input_ec.read(&mut self.input_event_rid);
+
+        let session_status = &*session_status;
+
+        if session_status != &SessionStatus::HostEstablished
+            && session_status != &SessionStatus::JoinEstablished
+        {
+            input_events
+                .filter_map(|input_event| GameInputEvent::try_from(input_event).ok())
+                .for_each(|game_input_event| game_input_ec.single_write(game_input_event));
+        }
     }
 }
