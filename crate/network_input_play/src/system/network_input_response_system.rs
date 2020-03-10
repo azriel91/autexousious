21ed5ccc02d0144255ae53dd768d57d852322b99
Azriel Hoh <mail@azriel.im>
2020-03-11 10:53:06 +1300

    Updated `NetworkInputResponseSystem` to read `GameInputEvent`s.
    Issue #199

--- crate/network_input_play/src/system/network_input_response_system.rs
+++ crate/network_input_play/src/system/network_input_response_system.rs
@@ -1,35 +1,34 @@
 use amethyst::{
     derive::SystemDesc,
     ecs::{Read, System, World, Write},
-    input::InputEvent,
     shred::{ResourceId, SystemData},
     shrev::{EventChannel, ReaderId},
 };
 use derivative::Derivative;
 use derive_new::new;
-use game_input_model::config::ControlBindings;
+use game_input_model::GameInputEvent;
 use log::debug;
 use net_model::play::{NetData, NetEventChannel};
 use network_session_model::play::SessionStatus;
 
-/// Writes received `InputEvent<ControlBindings>`s from the net channel to the regular event channel.
+/// Writes received `GameInputEvent`s from the net channel to the regular event channel.
 #[derive(Debug, SystemDesc, new)]
 #[system_desc(name(NetworkInputResponseSystemDesc))]
 pub struct NetworkInputResponseSystem {
-    /// Reader ID for the `InputEvent<ControlBindings>` channel.
+    /// Reader ID for the `GameInputEvent` channel.
     #[system_desc(event_channel_reader)]
-    network_input_event_rid: ReaderId<NetData<InputEvent<ControlBindings>>>,
+    game_input_event_rid: ReaderId<NetData<GameInputEvent>>,
 }
 
 #[derive(Derivative, SystemData)]
 #[derivative(Debug)]
 pub struct NetworkInputResponseSystemData<'s> {
-    /// Net `InputEvent<ControlBindings>` channel.
+    /// Net `GameInputEvent` channel.
     #[derivative(Debug = "ignore")]
-    pub network_input_nec: Read<'s, NetEventChannel<InputEvent<ControlBindings>>>,
-    /// `InputEvent<ControlBindings>` channel.
+    pub game_input_nec: Read<'s, NetEventChannel<GameInputEvent>>,
+    /// `GameInputEvent` channel.
     #[derivative(Debug = "ignore")]
-    pub input_ec: Write<'s, EventChannel<InputEvent<ControlBindings>>>,
+    pub game_input_ec: Write<'s, EventChannel<GameInputEvent>>,
     /// `SessionStatus` resource.
     #[derivative(Debug = "ignore")]
     pub session_status: Read<'s, SessionStatus>,
@@ -41,25 +40,26 @@ impl<'s> System<'s> for NetworkInputResponseSystem {
     fn run(
         &mut self,
         NetworkInputResponseSystemData {
-            network_input_nec,
-            mut input_ec,
+            game_input_nec,
+            mut game_input_ec,
             session_status,
         }: Self::SystemData,
     ) {
-        let network_input_events = network_input_nec.read(&mut self.network_input_event_rid);
+        let game_input_events = game_input_nec.read(&mut self.game_input_event_rid);
         let session_status = &*session_status;
 
         if session_status == &SessionStatus::JoinEstablished
             || session_status == &SessionStatus::HostEstablished
         {
-            network_input_events.for_each(|ev| {
+            game_input_events.for_each(|ev| {
                 let NetData {
-                    data: input_event, ..
+                    data: game_input_event,
+                    ..
                 } = ev;
 
-                debug!("`NetData<InputEvent>` received.");
+                debug!("`NetData<GameInputEvent>` received.");
 
-                input_ec.single_write(input_event.clone());
+                game_input_ec.single_write(*game_input_event);
             });
         }
     }
--- crate/workspace_tests/src/network_input_play/system/network_input_response_system.rs
+++ crate/workspace_tests/src/network_input_play/system/network_input_response_system.rs
@@ -4,25 +4,29 @@ mod tests {
 
     use amethyst::{
         ecs::{Read, SystemData, World, WorldExt, WriteExpect},
-        input::InputEvent,
         shrev::{EventChannel, ReaderId},
         Error,
     };
     use amethyst_test::AmethystApplication;
-    use game_input_model::config::{ControlAction, ControlBindings, PlayerActionControl};
+    use game_input_model::{
+        config::{ControlAction, PlayerActionControl},
+        GameInputEvent,
+    };
     use net_model::play::{NetData, NetEventChannel};
     use network_session_model::play::SessionStatus;
 
     use network_input_play::NetworkInputResponseSystemDesc;
 
     #[test]
-    fn does_nothing_when_no_input_event() -> Result<(), Error> {
+    fn does_nothing_when_no_game_input_event() -> Result<(), Error> {
         run_test(
             SetupParams {
                 session_status: SessionStatus::None,
-                input_event: None,
+                game_input_event: None,
             },
-            ExpectedParams { input_event: None },
+            ExpectedParams {
+                game_input_event: None,
+            },
         )
     }
 
@@ -31,13 +35,13 @@ mod tests {
         run_test(
             SetupParams {
                 session_status: SessionStatus::HostEstablished,
-                input_event: Some(InputEvent::ActionPressed(PlayerActionControl::new(
+                game_input_event: Some(GameInputEvent::ActionPressed(PlayerActionControl::new(
                     0,
                     ControlAction::Attack,
                 ))),
             },
             ExpectedParams {
-                input_event: Some(InputEvent::ActionPressed(PlayerActionControl::new(
+                game_input_event: Some(GameInputEvent::ActionPressed(PlayerActionControl::new(
                     0,
                     ControlAction::Attack,
                 ))),
@@ -48,53 +52,53 @@ mod tests {
     fn run_test(
         SetupParams {
             session_status: session_status_setup,
-            input_event,
+            game_input_event,
         }: SetupParams,
         ExpectedParams {
-            input_event: input_event_expected,
+            game_input_event: game_input_event_expected,
         }: ExpectedParams,
     ) -> Result<(), Error> {
         AmethystApplication::blank()
-            .with_setup(<Read<'_, EventChannel<InputEvent<ControlBindings>>> as SystemData>::setup)
-            .with_setup(setup_input_event_reader)
+            .with_setup(<Read<'_, EventChannel<GameInputEvent>> as SystemData>::setup)
+            .with_setup(setup_game_input_event_reader)
             .with_system_desc(NetworkInputResponseSystemDesc::default(), "", &[])
             .with_resource(session_status_setup)
             .with_effect(move |world| {
-                if let Some(input_event) = input_event {
+                if let Some(game_input_event) = game_input_event {
                     let socket_addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 1234));
                     world
-                        .write_resource::<NetEventChannel<InputEvent<ControlBindings>>>()
+                        .write_resource::<NetEventChannel<GameInputEvent>>()
                         .single_write(NetData {
                             socket_addr,
-                            data: input_event,
+                            data: game_input_event,
                         });
                 }
             })
             .with_assertion(move |world| {
-                let (mut input_event_rid, network_input_ec) = world.system_data::<(
-                    WriteExpect<'_, ReaderId<InputEvent<ControlBindings>>>,
-                    Read<'_, EventChannel<InputEvent<ControlBindings>>>,
+                let (mut game_input_event_rid, network_input_ec) = world.system_data::<(
+                    WriteExpect<'_, ReaderId<GameInputEvent>>,
+                    Read<'_, EventChannel<GameInputEvent>>,
                 )>();
-                let input_event = network_input_ec.read(&mut *input_event_rid).next();
+                let game_input_event = network_input_ec.read(&mut *game_input_event_rid).next();
 
-                assert_eq!(input_event_expected.as_ref(), input_event);
+                assert_eq!(game_input_event_expected.as_ref(), game_input_event);
             })
             .run()
     }
 
-    fn setup_input_event_reader(world: &mut World) {
-        let input_event_rid = world
-            .write_resource::<EventChannel<InputEvent<ControlBindings>>>()
+    fn setup_game_input_event_reader(world: &mut World) {
+        let game_input_event_rid = world
+            .write_resource::<EventChannel<GameInputEvent>>()
             .register_reader();
-        world.insert(input_event_rid);
+        world.insert(game_input_event_rid);
     }
 
     struct SetupParams {
         session_status: SessionStatus,
-        input_event: Option<InputEvent<ControlBindings>>,
+        game_input_event: Option<GameInputEvent>,
     }
 
     struct ExpectedParams {
-        input_event: Option<InputEvent<ControlBindings>>,
+        game_input_event: Option<GameInputEvent>,
     }
 }
