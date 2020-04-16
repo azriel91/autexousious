9cce5a50d1d2cfbe0b69d1332ff6bc64156ab774
Azriel Hoh <mail@azriel.im>
2020-04-16 16:09:43 +1200

    Use `ThreadPool` from `World` instead of spawning a thread.

--- crate/stdio_input/src/stdio_input_bundle.rs
+++ crate/stdio_input/src/stdio_input_bundle.rs
@@ -1,13 +1,13 @@
 use std::any;
 
 use amethyst::{
-    core::bundle::SystemBundle,
+    core::{bundle::SystemBundle, SystemDesc},
     ecs::{DispatcherBuilder, World},
     Error,
 };
 use derive_new::new;
 
-use crate::StdinSystem;
+use crate::{StdinSystem, StdinSystemDesc};
 
 /// Adds the `StdinSystem` to the `World`.
 #[derive(Debug, new)]
@@ -16,10 +16,14 @@ pub struct StdioInputBundle;
 impl<'a, 'b> SystemBundle<'a, 'b> for StdioInputBundle {
     fn build(
         self,
-        _world: &mut World,
+        world: &mut World,
         builder: &mut DispatcherBuilder<'a, 'b>,
     ) -> Result<(), Error> {
-        builder.add(StdinSystem::new(), any::type_name::<StdinSystem>(), &[]);
+        builder.add(
+            StdinSystemDesc::new().build(world),
+            any::type_name::<StdinSystem>(),
+            &[],
+        );
         Ok(())
     }
 }
